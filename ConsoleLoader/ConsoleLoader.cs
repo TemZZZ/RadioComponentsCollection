1ac4f5f667e6aa6926961f5945c0cda880301835
TemZZZ <temsky.v.tomske@gmail.com>
2020-04-26 23:26:58 +0700

    fixture(ConsoleLoader): переписана логика консольной программы
    - функции логики программы вынесены в файл ConsoleLoader.cs
    - точка входа вынесена в файл Main.cs

---
+++ ConsoleLoader/ConsoleLoader.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+using Lab1Model;
+using Lab1Model.PassiveComponents;
+
+
+namespace ConsoleLoaderModel
+{
+    public delegate void Printer(string message);
+
+    public static class ConsoleLoader
+    {
+        public static ComponentBase GetComponent(string type,
+            Printer printer=null)
+        {
+            switch (type.ToUpper())
+            {
+                case "R":
+                    return new Resistor();
+
+                case "L":
+                    return new Inductor();
+
+                case "C":
+                    return new Capacitor();
+
+                default:
+                    printer?.Invoke("Неизвестный тип компонента.\n" +
+                        "Допустимые R, L или C");
+
+                    return null;
+            }
+        }
+
+        public static double StringToDouble(string inputStr,
+            Printer printer = null)
+        {
+            double value = 0;
+
+            try
+            {
+                value = Convert.ToDouble(inputStr.Replace('.', ','));
+            }
+            catch (FormatException)
+            {
+                printer?.Invoke("Введенная строка не соответствует " +
+                    "формату вещественного числа.\n" +
+                    "Введите число вида X.Y или X,Y, где X и Y - наборы цифр.");
+
+                return double.NaN;
+            }
+
+            if ((value < double.MinValue) || (value > double.MaxValue))
+            {
+                printer?.Invoke("Введенное число не укладывается в диапазон " +
+                    "вещественных чисел двойной точности.\n" +
+                    "Введите чило из диапазона от " +
+                    "(-/+)5.0*10^(-324) до (-/+)1.7*10^308.");
+
+                return double.NaN;
+            }
+
+            return value;
+        }
+
+        public static void AskComponentValue(in ComponentBase cmp,
+            Printer printer)
+        {
+            if (cmp is Resistor)
+            {
+                printer("Введите сопротивление резистора в омах: ");
+            }
+            else if (cmp is Inductor)
+            {
+                printer("Введите индуктивность катушки в генри: ");
+            }
+            else if (cmp is Capacitor)
+            {
+                printer("Введите емкость конденсатора в фарадах: ");
+            }
+            else
+            {
+                printer("Это не радиокомпонент");
+            }
+        }
+    }
+}
---
+++ ConsoleLoader/Main.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+using ConsoleLoaderModel;
+
+using Lab1Model;
+
+
+public class MainApp
+{
+	public static void Main()
+	{
+		string inputStr = default;
+
+		while (true)
+		{
+			Console.Write("\nДля выхода из программы введите Q или\n" +
+				"введите тип радиокомпонента (R, L или C): ");
+
+			inputStr = Console.ReadLine();
+
+			if (inputStr.ToUpper() == "Q")
+			{
+				return;
+			}
+
+			ComponentBase cmp = default;
+			cmp = ConsoleLoader.GetComponent(inputStr, Console.WriteLine);
+
+			if (cmp != null)
+			{
+				ConsoleLoader.AskComponentValue(in cmp, Console.Write);
+				inputStr = Console.ReadLine();
+
+				double value;
+				value = ConsoleLoader.StringToDouble(inputStr, Console.WriteLine);
+
+				if (value < 0)
+				{
+					Console.WriteLine("Значение физической величины " +
+						"не может быть меньше нуля.");
+
+					continue;
+				}
+
+				if (value == double.NaN)
+				{
+					continue;
+				}
+
+				Console.Write("Введите частоту в герцах: ");
+				inputStr = Console.ReadLine();
+
+				double freq;
+				freq = ConsoleLoader.StringToDouble(inputStr, Console.WriteLine);
+
+				if (freq < 0)
+				{
+					Console.WriteLine("Значение частоты " +
+						"не может быть меньше нуля.");
+
+					continue;
+				}
+
+				if (freq == double.NaN)
+				{
+					continue;
+				}
+
+				cmp.Value = value;
+
+				double im = cmp.GetImpedance(freq).Imaginary;
+
+				char sign = '+';
+
+				if (im < 0)
+				{
+					sign = '-';
+				}
+
+				Console.WriteLine($"Импеданс равен " +
+					$"{cmp.GetImpedance(freq).Real} {sign} " +
+					$"{Math.Abs(im)}j Ом");
+			}
+		}
+	}
+}
