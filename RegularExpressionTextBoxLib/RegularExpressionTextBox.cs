ecdcd16571dd74afc2c725ecc8b6dc750b94a816
TemZZZ <temsky.v.tomske@gmail.com>
2020-04-29 14:03:58 +0700

    fixture(RegularExpressionTextBox): добавлено автосвойство регулярного выражения

--- RegularExpressionTextBoxLib/RegularExpressionTextBox.cs
+++ RegularExpressionTextBoxLib/RegularExpressionTextBox.cs
@@ -7,12 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 
 namespace RegularExpressionTextBoxLib
 {
+    /// <summary>
+    /// Представляет элемент управления Windows текстового поля
+    /// с проверкой на соответствие введенных данных
+    /// регулярному выражению
+    /// </summary>
     public partial class RegularExpressionTextBox: TextBox
     {
+        /// <summary>
+        /// Регулярное выражение для проверки введенных данных
+        /// </summary>
+        public Regex RegularExpression { get; set; }
         public RegularExpressionTextBox()
         {
             InitializeComponent();
