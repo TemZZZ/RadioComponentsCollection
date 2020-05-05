f3565a144d2fb45b981d2d32fcf4b88e40cdeb88
TemZZZ <temsky.v.tomske@gmail.com>
2020-05-05 22:39:10 +0700

    fix(MainForm): исправлены шаблоны регулярных выражений doublePattern и positiveDoublePattern

--- Lab1View/MainForm.cs
+++ Lab1View/MainForm.cs
@@ -21,10 +21,10 @@ namespace Lab1View
     public partial class MainForm : Form
     {
         const string doublePattern =
-            @"^[-+]?[0-9]*[\.\,]?[0-9]*([eE]?[-+]?[0-9]*)?$";
+            @"^[-+]?([0-9]+[\.\,]?[0-9]*([eE]?[-+]?[0-9]*))?$";
 
         const string positiveDoublePattern =
-            @"^[0-9]+[\.\,]?[0-9]*([eE]?[-+]?[0-9]*)?$";
+            @"^([0-9]+[\.\,]?[0-9]*([eE]?[-+]?[0-9]*))?$";
 
         public MainForm()
         {
