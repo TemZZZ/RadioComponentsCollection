0dc298afbe66878ca6acf6f294c68b4b0b55b14f
TemZZZ <temsky.v.tomske@gmail.com>
2020-05-05 11:19:58 +0700

    style(Main): перегруппированы строки
    - перегруппированы строки для лучшей прослеживаемости логики
    - константа Q занесена в метод Main
    - отдельный блок кода в начале цикла исключен

--- ConsoleLoader/Main.cs
+++ ConsoleLoader/Main.cs
@@ -5,56 +5,42 @@ using Lab1Model;
 
 public class MainApp
 {
-	/// <summary>Символ выхода из программы</summary>
-	const string exitCharacter = "Q";
-
 	public static void Main()
 	{
+		/// <summary>Символ выхода из программы</summary>
+		const string exitCharacter = "Q";
+
 		Console.WriteLine("Программа для вычисления\n" +
 			"комплексного сопротивления радиокомпонентов");
 
 		while (true)
 		{
-			ComponentBase component;
-
-			{
-				Console.Write("\nДля выхода из программы введите Q (q)\n" +
+			Console.Write("\nДля выхода из программы введите Q (q)\n" +
 					"или введите тип радиокомпонента R (r), L (l) или C (c): ");
+			string userAnswer = Console.ReadLine();
+			if (userAnswer.ToUpper() == exitCharacter) { return; }
 
-				string userAnswer = Console.ReadLine();
-
-				if (userAnswer.ToUpper() == exitCharacter) { return; }
-
-				component = ConsoleLoader.GetComponent(userAnswer,
-					Console.WriteLine);
-			}
-
+			ComponentBase component = ConsoleLoader.GetComponent(
+				userAnswer,	Console.WriteLine);
 			if (component == null) { continue; }
 
 			ConsoleLoader.AskComponentValue(in component, Console.Write);
-
 			double value = ConsoleLoader.StringToDouble(Console.ReadLine(),
 				Console.WriteLine);
-
 			if (double.IsNaN(value) || double.IsInfinity(value)) { continue; }
-
 			if (!ConsoleLoader.IsPositive(value,
 				"Значение физической величины не может быть отрицательным",
 				Console.WriteLine)) { continue; }
 
 			Console.Write("Введите частоту в герцах: ");
-
 			double freq = ConsoleLoader.StringToDouble(Console.ReadLine(),
 				Console.WriteLine);
-
 			if (double.IsNaN(freq) || double.IsInfinity(freq)) { continue; }
-
 			if (!ConsoleLoader.IsPositive(freq,
 				"Значение частоты не может быть отрицательным",
 				Console.WriteLine)) { continue; }
 
 			component.Value = value;
-
 			ConsoleLoader.PrintComplex(component.GetImpedance(freq),
 				Console.WriteLine);
 		}
