60089380e22a5bd106b8d7f642feb483469fe249
TemZZZ <temsky.v.tomske@gmail.com>
2020-05-07 23:12:46 +0700

    feature(MainForm): тип списка radioComponents изменен с List на BindingList
    feature(MainForm): изменен модификатор доступа к списку radioComponents с private на internal static

--- Lab1View/MainForm.cs
+++ Lab1View/MainForm.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.ComponentModel;
 using System.Windows.Forms;
 using Lab1Model;
 
@@ -11,7 +11,7 @@ namespace Lab1View
         /// <summary>
         /// Список радиокомпонентов
         /// </summary>
-        private List<RadioComponentBase> radioComponents;
+        internal static BindingList<RadioComponentBase> radioComponents;
 
         AddRadioComponentForm addRadioComponentForm;
 
