8f7966a02c768fa518288aea23cf689a554fbf5e
TemZZZ <temsky.v.tomske@gmail.com>
2020-05-10 01:05:57 +0700

    feature(MainForm): изменена форма
    - добавлены кнопки для сохранения в файл/загрузки из файлов радиокомпонентов
    - добавлено поле для ввода частоты
    - добавлено поле для вывода импеданса
    - добавлена кнопка для поиска

--- Lab1View/MainForm.Designer.cs
+++ Lab1View/MainForm.Designer.cs
@@ -32,19 +32,29 @@
             this.radioComponentsDataGridView = new System.Windows.Forms.DataGridView();
             this.addRadioComponentButton = new System.Windows.Forms.Button();
             this.deleteRadioComponentButton = new System.Windows.Forms.Button();
+            this.saveToFileButton = new System.Windows.Forms.Button();
+            this.loadFromFileButton = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.frequencyLabel = new System.Windows.Forms.Label();
+            this.impedanceLabel = new System.Windows.Forms.Label();
+            this.frequencyPositiveDoubleTextBox = new PositiveDoubleTextBoxLib.PositiveDoubleTextBox();
+            this.impedanceTextBox = new System.Windows.Forms.TextBox();
+            this.impedanceGroupBox = new System.Windows.Forms.GroupBox();
+            this.addDeleteComponentGroupBox = new System.Windows.Forms.GroupBox();
+            this.workWithFilesGroupBox = new System.Windows.Forms.GroupBox();
             this.radioComponentsGroupBox.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.radioComponentsDataGridView)).BeginInit();
+            this.impedanceGroupBox.SuspendLayout();
+            this.addDeleteComponentGroupBox.SuspendLayout();
+            this.workWithFilesGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // radioComponentsGroupBox
             // 
-            this.radioComponentsGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             this.radioComponentsGroupBox.Controls.Add(this.radioComponentsDataGridView);
             this.radioComponentsGroupBox.Location = new System.Drawing.Point(12, 12);
             this.radioComponentsGroupBox.Name = "radioComponentsGroupBox";
-            this.radioComponentsGroupBox.Size = new System.Drawing.Size(652, 332);
+            this.radioComponentsGroupBox.Size = new System.Drawing.Size(684, 332);
             this.radioComponentsGroupBox.TabIndex = 0;
             this.radioComponentsGroupBox.TabStop = false;
             this.radioComponentsGroupBox.Text = "Радиокомпоненты";
@@ -52,23 +62,19 @@
             // radioComponentsDataGridView
             // 
             this.radioComponentsDataGridView.AllowUserToOrderColumns = true;
-            this.radioComponentsDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             this.radioComponentsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.radioComponentsDataGridView.Location = new System.Drawing.Point(6, 19);
             this.radioComponentsDataGridView.Name = "radioComponentsDataGridView";
             this.radioComponentsDataGridView.ReadOnly = true;
             this.radioComponentsDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.radioComponentsDataGridView.Size = new System.Drawing.Size(639, 303);
+            this.radioComponentsDataGridView.Size = new System.Drawing.Size(672, 303);
             this.radioComponentsDataGridView.TabIndex = 0;
             // 
             // addRadioComponentButton
             // 
-            this.addRadioComponentButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.addRadioComponentButton.Location = new System.Drawing.Point(211, 350);
+            this.addRadioComponentButton.Location = new System.Drawing.Point(6, 19);
             this.addRadioComponentButton.Name = "addRadioComponentButton";
-            this.addRadioComponentButton.Size = new System.Drawing.Size(124, 23);
+            this.addRadioComponentButton.Size = new System.Drawing.Size(161, 23);
             this.addRadioComponentButton.TabIndex = 0;
             this.addRadioComponentButton.Text = "Добавить компонент";
             this.addRadioComponentButton.UseVisualStyleBackColor = true;
@@ -76,27 +82,129 @@
             // 
             // deleteRadioComponentButton
             // 
-            this.deleteRadioComponentButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.deleteRadioComponentButton.Location = new System.Drawing.Point(341, 350);
+            this.deleteRadioComponentButton.Location = new System.Drawing.Point(6, 48);
             this.deleteRadioComponentButton.Name = "deleteRadioComponentButton";
-            this.deleteRadioComponentButton.Size = new System.Drawing.Size(124, 23);
+            this.deleteRadioComponentButton.Size = new System.Drawing.Size(161, 23);
             this.deleteRadioComponentButton.TabIndex = 1;
             this.deleteRadioComponentButton.Text = "Удалить компонент";
             this.deleteRadioComponentButton.UseVisualStyleBackColor = true;
             this.deleteRadioComponentButton.Click += new System.EventHandler(this.DeleteRadioComponentButton_Click);
             // 
+            // saveToFileButton
+            // 
+            this.saveToFileButton.Location = new System.Drawing.Point(6, 19);
+            this.saveToFileButton.Name = "saveToFileButton";
+            this.saveToFileButton.Size = new System.Drawing.Size(161, 23);
+            this.saveToFileButton.TabIndex = 2;
+            this.saveToFileButton.Text = "Сохранить в файл";
+            this.saveToFileButton.UseVisualStyleBackColor = true;
+            // 
+            // loadFromFileButton
+            // 
+            this.loadFromFileButton.Location = new System.Drawing.Point(6, 48);
+            this.loadFromFileButton.Name = "loadFromFileButton";
+            this.loadFromFileButton.Size = new System.Drawing.Size(161, 23);
+            this.loadFromFileButton.TabIndex = 3;
+            this.loadFromFileButton.Text = "Загрузить из файла";
+            this.loadFromFileButton.UseVisualStyleBackColor = true;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(612, 383);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(84, 23);
+            this.searchButton.TabIndex = 4;
+            this.searchButton.Text = "Поиск";
+            this.searchButton.UseVisualStyleBackColor = true;
+            // 
+            // frequencyLabel
+            // 
+            this.frequencyLabel.AutoSize = true;
+            this.frequencyLabel.Location = new System.Drawing.Point(20, 24);
+            this.frequencyLabel.Name = "frequencyLabel";
+            this.frequencyLabel.Size = new System.Drawing.Size(70, 13);
+            this.frequencyLabel.TabIndex = 5;
+            this.frequencyLabel.Text = "Частота, Гц:";
+            // 
+            // impedanceLabel
+            // 
+            this.impedanceLabel.AutoSize = true;
+            this.impedanceLabel.Location = new System.Drawing.Point(6, 53);
+            this.impedanceLabel.Name = "impedanceLabel";
+            this.impedanceLabel.Size = new System.Drawing.Size(84, 13);
+            this.impedanceLabel.TabIndex = 6;
+            this.impedanceLabel.Text = "Импеданс, Ом:";
+            // 
+            // frequencyPositiveDoubleTextBox
+            // 
+            this.frequencyPositiveDoubleTextBox.Location = new System.Drawing.Point(96, 20);
+            this.frequencyPositiveDoubleTextBox.Name = "frequencyPositiveDoubleTextBox";
+            this.frequencyPositiveDoubleTextBox.Pattern = "^([0-9]+[\\.\\,]?[0-9]*([eE]?[-+]?[0-9]*))?$";
+            this.frequencyPositiveDoubleTextBox.Size = new System.Drawing.Size(130, 20);
+            this.frequencyPositiveDoubleTextBox.TabIndex = 7;
+            this.frequencyPositiveDoubleTextBox.Text = "0";
+            // 
+            // impedanceTextBox
+            // 
+            this.impedanceTextBox.Location = new System.Drawing.Point(96, 49);
+            this.impedanceTextBox.Name = "impedanceTextBox";
+            this.impedanceTextBox.ReadOnly = true;
+            this.impedanceTextBox.Size = new System.Drawing.Size(130, 20);
+            this.impedanceTextBox.TabIndex = 8;
+            // 
+            // impedanceGroupBox
+            // 
+            this.impedanceGroupBox.Controls.Add(this.impedanceLabel);
+            this.impedanceGroupBox.Controls.Add(this.impedanceTextBox);
+            this.impedanceGroupBox.Controls.Add(this.frequencyLabel);
+            this.impedanceGroupBox.Controls.Add(this.frequencyPositiveDoubleTextBox);
+            this.impedanceGroupBox.Location = new System.Drawing.Point(12, 350);
+            this.impedanceGroupBox.Name = "impedanceGroupBox";
+            this.impedanceGroupBox.Size = new System.Drawing.Size(234, 78);
+            this.impedanceGroupBox.TabIndex = 9;
+            this.impedanceGroupBox.TabStop = false;
+            this.impedanceGroupBox.Text = "Импеданс";
+            // 
+            // addDeleteComponentGroupBox
+            // 
+            this.addDeleteComponentGroupBox.Controls.Add(this.addRadioComponentButton);
+            this.addDeleteComponentGroupBox.Controls.Add(this.deleteRadioComponentButton);
+            this.addDeleteComponentGroupBox.Location = new System.Drawing.Point(252, 350);
+            this.addDeleteComponentGroupBox.Name = "addDeleteComponentGroupBox";
+            this.addDeleteComponentGroupBox.Size = new System.Drawing.Size(174, 78);
+            this.addDeleteComponentGroupBox.TabIndex = 10;
+            this.addDeleteComponentGroupBox.TabStop = false;
+            this.addDeleteComponentGroupBox.Text = "Добавить/удалить компонент";
+            // 
+            // workWithFilesGroupBox
+            // 
+            this.workWithFilesGroupBox.Controls.Add(this.saveToFileButton);
+            this.workWithFilesGroupBox.Controls.Add(this.loadFromFileButton);
+            this.workWithFilesGroupBox.Location = new System.Drawing.Point(432, 350);
+            this.workWithFilesGroupBox.Name = "workWithFilesGroupBox";
+            this.workWithFilesGroupBox.Size = new System.Drawing.Size(174, 78);
+            this.workWithFilesGroupBox.TabIndex = 11;
+            this.workWithFilesGroupBox.TabStop = false;
+            this.workWithFilesGroupBox.Text = "Работа с файлами";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(676, 381);
-            this.Controls.Add(this.deleteRadioComponentButton);
+            this.ClientSize = new System.Drawing.Size(707, 439);
+            this.Controls.Add(this.workWithFilesGroupBox);
+            this.Controls.Add(this.addDeleteComponentGroupBox);
+            this.Controls.Add(this.impedanceGroupBox);
+            this.Controls.Add(this.searchButton);
             this.Controls.Add(this.radioComponentsGroupBox);
-            this.Controls.Add(this.addRadioComponentButton);
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.radioComponentsGroupBox.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.radioComponentsDataGridView)).EndInit();
+            this.impedanceGroupBox.ResumeLayout(false);
+            this.impedanceGroupBox.PerformLayout();
+            this.addDeleteComponentGroupBox.ResumeLayout(false);
+            this.workWithFilesGroupBox.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -107,6 +215,16 @@
         private System.Windows.Forms.DataGridView radioComponentsDataGridView;
         private System.Windows.Forms.Button addRadioComponentButton;
         private System.Windows.Forms.Button deleteRadioComponentButton;
+        private System.Windows.Forms.Button saveToFileButton;
+        private System.Windows.Forms.Button loadFromFileButton;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Label frequencyLabel;
+        private System.Windows.Forms.Label impedanceLabel;
+        private PositiveDoubleTextBoxLib.PositiveDoubleTextBox frequencyPositiveDoubleTextBox;
+        private System.Windows.Forms.TextBox impedanceTextBox;
+        private System.Windows.Forms.GroupBox impedanceGroupBox;
+        private System.Windows.Forms.GroupBox addDeleteComponentGroupBox;
+        private System.Windows.Forms.GroupBox workWithFilesGroupBox;
     }
 }
 
