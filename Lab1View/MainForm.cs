7602a847585fd69abea1a68600023faca6cb73c0
TemZZZ <temsky.v.tomske@gmail.com>
2020-05-12 15:01:19 +0700

    style(MainForm): добавлены фигурные скобки в блоках if

--- Lab1View/MainForm.cs
+++ Lab1View/MainForm.cs
@@ -211,13 +211,19 @@ namespace Lab1View
                 Math.Abs(number.Imaginary).ToString(format);
 
             if (double.IsInfinity(number.Real))
+            {
                 realString = infinityString;
+            }
             if (double.IsInfinity(number.Imaginary))
+            {
                 absImaginaryString = infinityString;
+            }
 
             char sign = signPlus;
             if (number.Imaginary < 0)
+            {
                 sign = signMinus;
+            }
 
             return $"{realString} {sign} {absImaginaryString}j";
         }
@@ -247,7 +253,9 @@ namespace Lab1View
             object sender, DataGridViewRowsAddedEventArgs e)
         {
             if (radioComponentsDataGridView.Rows.Count == 1)
+            {
                 radioComponentsDataGridView.Rows[0].Selected = true;
+            }
         }
 
         private void SaveToFileButton_Click(object sender, EventArgs e)
