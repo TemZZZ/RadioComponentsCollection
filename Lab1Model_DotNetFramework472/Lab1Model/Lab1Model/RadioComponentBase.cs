55c6f30d289075929cfc330b7063fd3715f54048
TemZZZ <temsky.v.tomske@gmail.com>
2020-05-12 11:17:53 +0700

    feature(RadioComponentBase): добавлены атрибуты для возможности сериализации объектов Resistor, Inductor и Capacitor

--- Lab1Model_DotNetFramework472/Lab1Model/Lab1Model/RadioComponentBase.cs
+++ Lab1Model_DotNetFramework472/Lab1Model/Lab1Model/RadioComponentBase.cs
@@ -1,9 +1,19 @@
 ﻿using System;
 using System.Numerics;
+using System.Xml.Serialization;
+using Lab1Model.PassiveComponents;
 
 
 namespace Lab1Model
 {
+    /// <summary>
+    /// Атрибуты для возможности сериализации объектов
+    /// типов <see cref="Resistor"/>, <see cref="Inductor"/>
+    /// и <see cref="Capacitor"/>
+    /// </summary>
+    [XmlInclude(typeof(Resistor))]
+    [XmlInclude(typeof(Inductor))]
+    [XmlInclude(typeof(Capacitor))]
     /// <summary>
     /// Абстрактный класс радиокомпонента.
     /// Его наследуют все производные классы радиокомпонентов
