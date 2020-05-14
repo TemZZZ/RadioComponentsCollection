c4915231ebdb34ba303b80ec514ace94373bbc26
TemZZZ <temsky.v.tomske@gmail.com>
2020-05-14 12:31:58 +0700

    feature(Lab1View): добавлена форма выбора параметров сохранения радиокомпонентов SetRadioComponentSaveOptionForm
    - добавлено перечисление опций сохранения RadioComponentSaveOption
    - добавлено событие подтверждения выбора опции RadioComponentReadyToSaveEventHandler
    - реализован класс данных события RadioComponentReadyToSaveEventArgs
    - реализовано подтверждения выбора опции сохранения кликом по кнопке OkButton_Click
    - реализовано закрытие формы кликом по кнопке CancelButton_Click

---
+++ Lab1View/SetRadioComponentSaveOptionForm.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Lab1View
+{
+	partial class SetRadioComponentSaveOptionForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.saveSelectedRadioButton = new System.Windows.Forms.RadioButton();
+			this.saveAllRadioButton = new System.Windows.Forms.RadioButton();
+			this.saveOptionsGroupBox = new System.Windows.Forms.GroupBox();
+			this.okButton = new System.Windows.Forms.Button();
+			this.cancelButton = new System.Windows.Forms.Button();
+			this.saveOptionsGroupBox.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// saveSelectedRadioButton
+			// 
+			this.saveSelectedRadioButton.AutoSize = true;
+			this.saveSelectedRadioButton.Location = new System.Drawing.Point(6, 42);
+			this.saveSelectedRadioButton.Name = "saveSelectedRadioButton";
+			this.saveSelectedRadioButton.Size = new System.Drawing.Size(279, 17);
+			this.saveSelectedRadioButton.TabIndex = 0;
+			this.saveSelectedRadioButton.Text = "Сохранить только выделенные радиокомпоненты";
+			this.saveSelectedRadioButton.UseVisualStyleBackColor = true;
+			// 
+			// saveAllRadioButton
+			// 
+			this.saveAllRadioButton.AutoSize = true;
+			this.saveAllRadioButton.Checked = true;
+			this.saveAllRadioButton.Location = new System.Drawing.Point(6, 19);
+			this.saveAllRadioButton.Name = "saveAllRadioButton";
+			this.saveAllRadioButton.Size = new System.Drawing.Size(195, 17);
+			this.saveAllRadioButton.TabIndex = 1;
+			this.saveAllRadioButton.TabStop = true;
+			this.saveAllRadioButton.Text = "Сохранить все радиокомпоненты";
+			this.saveAllRadioButton.UseVisualStyleBackColor = true;
+			// 
+			// saveOptionsGroupBox
+			// 
+			this.saveOptionsGroupBox.Controls.Add(this.saveAllRadioButton);
+			this.saveOptionsGroupBox.Controls.Add(this.saveSelectedRadioButton);
+			this.saveOptionsGroupBox.Location = new System.Drawing.Point(12, 12);
+			this.saveOptionsGroupBox.Name = "saveOptionsGroupBox";
+			this.saveOptionsGroupBox.Size = new System.Drawing.Size(291, 67);
+			this.saveOptionsGroupBox.TabIndex = 2;
+			this.saveOptionsGroupBox.TabStop = false;
+			this.saveOptionsGroupBox.Text = "Настройки сохранения";
+			// 
+			// okButton
+			// 
+			this.okButton.Location = new System.Drawing.Point(78, 85);
+			this.okButton.Name = "okButton";
+			this.okButton.Size = new System.Drawing.Size(75, 23);
+			this.okButton.TabIndex = 3;
+			this.okButton.Text = "OK";
+			this.okButton.UseVisualStyleBackColor = true;
+			this.okButton.Click += new System.EventHandler(this.OkButton_Click);
+			// 
+			// cancelButton
+			// 
+			this.cancelButton.Location = new System.Drawing.Point(159, 85);
+			this.cancelButton.Name = "cancelButton";
+			this.cancelButton.Size = new System.Drawing.Size(75, 23);
+			this.cancelButton.TabIndex = 4;
+			this.cancelButton.Text = "Отмена";
+			this.cancelButton.UseVisualStyleBackColor = true;
+			this.cancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+			// 
+			// SaveRadioComponentForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(314, 118);
+			this.Controls.Add(this.cancelButton);
+			this.Controls.Add(this.okButton);
+			this.Controls.Add(this.saveOptionsGroupBox);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "SaveRadioComponentForm";
+			this.Text = "Сохранение радиокомпонентов";
+			this.saveOptionsGroupBox.ResumeLayout(false);
+			this.saveOptionsGroupBox.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.RadioButton saveSelectedRadioButton;
+		private System.Windows.Forms.RadioButton saveAllRadioButton;
+		private System.Windows.Forms.GroupBox saveOptionsGroupBox;
+		private System.Windows.Forms.Button okButton;
+		private System.Windows.Forms.Button cancelButton;
+	}
+}
---
+++ Lab1View/SetRadioComponentSaveOptionForm.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+namespace Lab1View
+{
+	/// <summary>
+	/// Форма выбора параметров сохранения радиокомпонентов
+	/// </summary>
+	public partial class SetRadioComponentSaveOptionForm : Form
+	{
+		/// <summary>
+		/// Событие, возникающее при выборе параметра сохранения
+		/// и подтверждении выбора
+		/// </summary>
+		public event RadioComponentReadyToSaveEventHandler
+			RadioComponentReadyToSave;
+
+		/// <summary>
+		/// Создает форму выбора параметров сохранения радиокомпонентов
+		/// </summary>
+		public SetRadioComponentSaveOptionForm()
+		{
+			InitializeComponent();
+		}
+
+		/// <summary>
+		/// Закрывает форму
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void CancelButton_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		/// <summary>
+		/// Подтверждает выбор параметра сохранения радиокнопкой:
+		/// сохранить все радиокомпоненты <see cref="saveAllRadioButton"/>
+		/// или сохранить выделенные радиокомпоненты
+		/// <see cref="saveSelectedRadioButton"/>.
+		/// Вызывает событие <see cref="RadioComponentReadyToSave"/>
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void OkButton_Click(object sender, EventArgs e)
+		{
+			RadioComponentSaveOption saveOption;
+			if (saveSelectedRadioButton.Checked)
+			{
+				saveOption = RadioComponentSaveOption.SaveSelected;
+			}
+			else
+			{
+				saveOption = RadioComponentSaveOption.SaveAll;
+			}
+			RadioComponentReadyToSave(this,
+				new RadioComponentReadyToSaveEventArgs(saveOption));
+		}
+	}
+
+	/// <summary>
+	/// Параметр сохранения радиокомпонентов
+	/// </summary>
+	public enum RadioComponentSaveOption
+	{
+		SaveAll,
+		SaveSelected
+	}
+
+	/// <summary>
+	/// Событие подтверждения выбора параметра сохранения радиокомпонента
+	/// </summary>
+	/// <param name="sender"></param>
+	/// <param name="e"></param>
+	public delegate void RadioComponentReadyToSaveEventHandler(
+		object sender, RadioComponentReadyToSaveEventArgs e);
+
+	/// <summary>
+	/// Класс данных события
+	/// <see cref="RadioComponentReadyToSaveEventHandler"/>
+	/// </summary>
+	public class RadioComponentReadyToSaveEventArgs : EventArgs
+    {
+		/// <summary>
+		/// Создает объект класса
+		/// <see cref="RadioComponentReadyToSaveEventArgs"/>
+		/// </summary>
+		/// <param name="radioComponentSaveOption">
+		/// Параметр сохранения радиокомпонентов</param>
+		public RadioComponentReadyToSaveEventArgs(
+			RadioComponentSaveOption radioComponentSaveOption)
+        {
+			RadioComponentSaveOption = radioComponentSaveOption;
+        }
+
+		/// <summary>
+		/// Позволяет получить параметр сохранения радиокомпонентов
+		/// </summary>
+        public RadioComponentSaveOption RadioComponentSaveOption { get; }
+    }
+}
