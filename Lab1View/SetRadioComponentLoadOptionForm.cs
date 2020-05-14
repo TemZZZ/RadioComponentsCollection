8db9102612dfa5da1cc46c3aceb8334232b9698e
TemZZZ <temsky.v.tomske@gmail.com>
2020-05-14 19:24:18 +0700

    feature(Lab1View): добавлена форма выбора параметров загрузки радиокомпонентов SetRadioComponentLoadOptionForm
    - добавлено перечисление опций загрузки RadioComponentLoadOption
    - добавлено событие подтверждения выбора опции RadioComponentReadyToLoadEventHandler
    - реализован класс данных события RadioComponentReadyToLoadEventArgs
    - реализовано подтверждение выбора опции загрузки кликом по кнопке OkButton_Click
    - реализовано закрытие формы кликом по кнопке CancelButton_Click

---
+++ Lab1View/SetRadioComponentLoadOptionForm.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+namespace Lab1View
+{
+	/// <summary>
+	/// Форма выбора параметров загрузки радиокомпонентов
+	/// </summary>
+	public partial class SetRadioComponentLoadOptionForm : Form
+	{
+		/// <summary>
+		/// Событие, возникающее после подтверждения выбора
+		/// параметра загрузки радиокомпонента
+		/// </summary>
+		public event RadioComponentReadyToLoadEventHandler
+			RadioComponentReadyToLoad;
+
+		/// <summary>
+		/// Создает форму выбора параметров загрузки радиокомпонентов
+		/// </summary>
+		public SetRadioComponentLoadOptionForm()
+		{
+			InitializeComponent();
+		}
+
+		/// <summary>
+		/// Подтверждает выбор параметра загрузки радиокнопкой:
+		/// добавить радиокомпоненты в конец таблицы
+		/// <see cref="addToEndRadioButton"/>
+		/// или заменить радиокомпоненты в таблице
+		/// <see cref="replaceAllRadioButton"/>.
+		/// Вызывает событие <see cref="RadioComponentReadyToLoad"/>
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void OkButton_Click(object sender, EventArgs e)
+		{
+			RadioComponentLoadOption loadOption;
+			if (replaceAllRadioButton.Checked)
+			{
+				loadOption = RadioComponentLoadOption.ReplaceAll;
+			}
+			else
+			{
+				loadOption = RadioComponentLoadOption.AddToEnd;
+			}
+			RadioComponentReadyToLoad(this,
+				new RadioComponentReadyToLoadEventArgs(loadOption));
+
+			this.Close();
+		}
+
+		/// <summary>
+		/// Закрывает форму
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void CancelButton_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+	}
+
+	/// <summary>
+	/// Параметр загрузки радиокомпонентов
+	/// </summary>
+	public enum RadioComponentLoadOption
+	{
+		AddToEnd,
+		ReplaceAll
+	}
+
+	/// <summary>
+	/// Событие подтверждения выбора параметра загрузки радиокомпонента
+	/// </summary>
+	/// <param name="sender"></param>
+	/// <param name="e"></param>
+	public delegate void RadioComponentReadyToLoadEventHandler(
+		object sender, RadioComponentReadyToLoadEventArgs e);
+
+	/// <summary>
+	/// Класс данных события
+	/// <see cref="RadioComponentReadyToLoadEventHandler"/>
+	/// </summary>
+	public class RadioComponentReadyToLoadEventArgs : EventArgs
+	{
+		/// <summary>
+		/// Создает объект класса
+		/// <see cref="RadioComponentReadyToLoadEventArgs"/>
+		/// </summary>
+		/// <param name="radioComponentLoadOption">
+		/// Параметр сохранения радиокомпонентов</param>
+		public RadioComponentReadyToLoadEventArgs(
+			RadioComponentLoadOption radioComponentLoadOption)
+		{
+			RadioComponentLoadOption = radioComponentLoadOption;
+		}
+
+		/// <summary>
+		/// Позволяет получить параметр загрузки радиокомпонентов
+		/// </summary>
+		public RadioComponentLoadOption RadioComponentLoadOption { get; }
+	}
+}
