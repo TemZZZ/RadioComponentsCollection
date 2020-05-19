b6021ef7fc260b9466b37b3acbe71c8f393d61ed
TemZZZ <temsky.v.tomske@gmail.com>
2020-05-19 22:34:06 +0700

    feature(SetRadioComponentLoadOptionForm.cs): класс RadioComponentReadyToLoadEventArgs перемещен в файл RadioComponentReadyToLoadEventArgs.cs

---
+++ Lab1View/RadioComponentReadyToLoadEventArgs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace Lab1View
+{
+	/// <summary>
+	/// Класс данных события подтверждения выбора
+	/// параметра загрузки радиокомпонента
+	/// </summary>
+	public class RadioComponentReadyToLoadEventArgs : EventArgs
+	{
+		/// <summary>
+		/// Создает объект класса
+		/// <see cref="RadioComponentReadyToLoadEventArgs"/>
+		/// </summary>
+		/// <param name="radioComponentLoadOption">
+		/// Параметр сохранения радиокомпонентов</param>
+		public RadioComponentReadyToLoadEventArgs(
+			RadioComponentLoadOption radioComponentLoadOption)
+		{
+			RadioComponentLoadOption = radioComponentLoadOption;
+		}
+
+		/// <summary>
+		/// Позволяет получить параметр загрузки радиокомпонентов
+		/// </summary>
+		public RadioComponentLoadOption RadioComponentLoadOption { get; }
+	}
+}
