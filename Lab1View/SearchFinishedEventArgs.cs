a5442d8377718ce56da74235aa72a6e4b4a556d5
TemZZZ <temsky.v.tomske@gmail.com>
2020-05-19 12:38:54 +0700

    feature(Lab1View): добавлен класс данных события завершения поиска
    - добавлен файл SearchFinishedEventArgs.cs
    - в файле реализован класс данных события завершения поиска SearchFinishedEventArgs

---
+++ Lab1View/SearchFinishedEventArgs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace Lab1View
+{
+	/// <summary>
+	/// Класс данных события завершения поиска
+	/// </summary>
+	public class SearchFinishedEventArgs : EventArgs
+	{
+		/// <summary>
+		/// Создает объект данных события завершения поиска
+		/// </summary>
+		/// <param name="foundIndices"></param>
+		public SearchFinishedEventArgs(int[] foundIndices)
+		{
+			FoundIndices = foundIndices;
+		}
+
+		/// <summary>
+		/// Индексы элементов, удовлетворяющих условиям поиска
+		/// </summary>
+		public int [] FoundIndices { get; }
+	}
+}
