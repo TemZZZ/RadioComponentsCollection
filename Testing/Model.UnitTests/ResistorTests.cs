8f477682ac19dc9d1c648585a36999cbc2fb8945
TemZZZ <temsky.v.tomske@gmail.com>
2020-05-26 17:46:06 +0700

    feature(Model.UnitTests): добавлен класс ResistorTests
    - реализован метод GetImpedance_GoodValuesAndFrequencies_ReturnsValue

---
+++ Testing/Model.UnitTests/ResistorTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+using NUnit.Framework;
+using Model.PassiveComponents;
+
+
+namespace Model.UnitTests
+{
+	/// <summary>
+	/// Набор тестов для класса <see cref="Resistor"/>
+	/// </summary>
+	class ResistorTests : RadioComponentBaseTests<Resistor>
+	{
+		/// <inheritdoc/>
+		public override
+			void GetImpedance_GoodValuesAndFrequencies_ReturnsValue(
+				double value, double frequency, Complex expectedImpedance)
+		{
+			// Setup
+			var resistor = new Resistor
+			{
+				Value = value
+			};
+
+			// Act
+			var actual = resistor.GetImpedance(frequency);
+
+			// Assert
+			Assert.Equals(actual, expectedImpedance);
+		}
+	}
+}
