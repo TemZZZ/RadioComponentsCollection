bc39cbdc5823b11b83c9c42410e8024140b265fa
TemZZZ <temsky.v.tomske@gmail.com>
2020-05-27 22:56:43 +0700

    test(Model.UnitTests.ResistorTests): изменены тесты метода GetImpedance
    - все тесты теперь формируются методом TestCasesForGetImpedanceMethod
    - набор тестов методу GetImpedance_GoodValuesAndFrequencies_ReturnsValue передается через атрибут [TestCaseSource("TestCasesForGetImpedanceMethod")]

--- Testing/Model.UnitTests/ResistorTests.cs
+++ Testing/Model.UnitTests/ResistorTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Numerics;
+﻿using System.Collections.Generic;
+using System.Numerics;
 using NUnit.Framework;
 using Model.PassiveComponents;
 
@@ -10,48 +11,49 @@ namespace Model.UnitTests
 	/// </summary>
 	class ResistorTests : RadioComponentBaseTests<Resistor>
 	{
-		static object[] TestCases =
+		/// <summary>
+		/// Формирует и возвращает перечислитель позитивных тестовых
+		/// случаев для метода <see cref="Resistor.GetImpedance(double)"/>
+		/// </summary>
+		/// <returns>Перечислитель наборов значений
+		/// "сопротивление, частота, импеданс"</returns>
+		static IEnumerable<TestCaseData> TestCasesForGetImpedanceMethod()
 		{
-			new object[] { MinRadioComponentValue, MinFrequency,
-				new Complex(MinRadioComponentValue, 0) },
+			double[] radioComponentValues =
+			{
+				MinRadioComponentValue,
+				MinRadioComponentValue + 1,
+				double.MaxValue - 1,
+				double.MaxValue
+			};
 
-			new object[] { MinRadioComponentValue + 1, MinFrequency,
-				new Complex(MinRadioComponentValue + 1, 0) },
+			double[] frequencies =
+			{
+				MinFrequency,
+				MinFrequency + 1,
+				double.MaxValue - 1,
+				double.MaxValue
+			};
 
-			new object[] { double.MaxValue, MinFrequency,
-				new Complex(double.MaxValue, 0) },
+			foreach (var radioComponentValue in radioComponentValues)
+			{
+				foreach (var frequency in frequencies)
+				{
+					var expectedImpedance
+						= new Complex(radioComponentValue, 0);
 
-			new object[] { MinRadioComponentValue, MinFrequency + 1,
-				new Complex(MinRadioComponentValue, 0) },
-
-			new object[] { MinRadioComponentValue + 1, MinFrequency + 1,
-				new Complex(MinRadioComponentValue + 1, 0) },
-
-			new object[] { double.MaxValue, MinFrequency + 1,
-				new Complex(double.MaxValue, 0) },
-
-			new object[] { MinRadioComponentValue, double.MaxValue - 1,
-				new Complex(MinRadioComponentValue, 0) },
-
-			new object[] { MinRadioComponentValue + 1, double.MaxValue - 1,
-				new Complex(MinRadioComponentValue + 1, 0) },
-
-			new object[] { double.MaxValue, double.MaxValue - 1,
-				new Complex(double.MaxValue, 0) },
-
-			new object[] { MinRadioComponentValue, double.MaxValue,
-				new Complex(MinRadioComponentValue, 0) },
-
-			new object[] { MinRadioComponentValue + 1, double.MaxValue,
-				new Complex(MinRadioComponentValue + 1, 0) },
-
-			new object[] { double.MaxValue, double.MaxValue,
-				new Complex(double.MaxValue, 0) }
-		};
+					yield return new TestCaseData(radioComponentValue,
+						frequency, expectedImpedance).SetName($"Когда " +
+						$"метод GetImpedance резистора со значением " +
+						$"сопротивления {radioComponentValue} вызывается " +
+						$"со значением частоты {frequency}, то  он должен " +
+						$"вернуть {expectedImpedance}");
+				}
+			}
+		}
 
 		/// <inheritdoc/>
-		[Test]
-		[TestCaseSource("TestCases")]
+		[Test, TestCaseSource("TestCasesForGetImpedanceMethod")]
 		public override
 			void GetImpedance_GoodValuesAndFrequencies_ReturnsValue(
 				double value, double frequency, Complex expectedImpedance)
