226473ee2531b24e98fae2bcfeda05df895816e5
TemZZZ <temsky.v.tomske@gmail.com>
2020-05-29 11:18:17 +0700

    feature(Model.UnitTests): добавлен тестовый класс InductorTests
    - реализован метод GetImpedanceMethodTestCases, формирующий тесты метода GetImpedance
    - реализован тестовый метод GetImpedance_GoodFrequencyForRadioComponentWithAssignedGoodValueToValueProperty_ReturnsExpectedImpedance
    - добавлены тесты метода GetImpedance: перед методом GetImpedance_GoodFrequencyForRadioComponentWithAssignedGoodValueToValueProperty_ReturnsExpectedImpedance добавлен атрибут [Test, TestCaseSource("GetImpedanceMethodTestCases")]

---
+++ Testing/Model.UnitTests/InductorTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using NUnit.Framework;
+using Model.PassiveComponents;
+
+
+namespace Model.UnitTests
+{
+	/// <summary>
+	/// Набор тестов для класса <see cref="Inductor"/>
+	/// </summary>
+	[TestFixture]
+	class InductorTests : IRadioComponentTestsBase<Inductor>
+	{
+		private static
+			IEnumerable<TestCaseData> GetImpedanceMethodTestCases()
+		{
+			foreach (var radioComponentValue in GoodRadioComponentValues)
+			{
+				foreach (var frequency in GoodFrequencies)
+				{
+					var expectedImpedance
+						= new Complex(0,
+							2 * Math.PI * frequency * radioComponentValue);
+
+					yield return new TestCaseData(frequency,
+						radioComponentValue, expectedImpedance).SetName(
+						$"Когда метод GetImpedance катушки индуктивности " +
+						$"со значением индуктивности " +
+						$"{radioComponentValue} вызывается со значением " +
+						$"частоты {frequency}, то он должен вернуть " +
+						$"{expectedImpedance}.");
+				}
+			}
+		}
+
+		[Test, TestCaseSource("GetImpedanceMethodTestCases")]
+		public override void
+			GetImpedance_GoodFrequencyForRadioComponentWithAssignedGoodValueToValueProperty_ReturnsExpectedImpedance(
+				double frequency, double radioComponentValue,
+				Complex expectedImpedance)
+		{
+			// Setup
+			var inductor = GetRadioComponent(radioComponentValue);
+
+			// Act
+			var actualImpedance = inductor.GetImpedance(frequency);
+
+			// Assert
+			Assert.AreEqual(actualImpedance, expectedImpedance);
+		}
+	}
+}
