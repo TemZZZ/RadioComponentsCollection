6c89e08334582462c5e1677a5e56bacce2c63670
TemZZZ <temsky.v.tomske@gmail.com>
2020-06-04 18:39:08 +0700

    feature(Model.UnitTests.InductorTests): изменена функциональность класса
    - класс теперь ничего не наследует
    - добавлено закрытое поле _radioComponentTests - объект класса RadioComponentTests<Inductor> для делегирования ему вызовов тестовых методов и методов-формирователей тестовых случаев классу RadioComponentTests
    - добавлены и реализованы публичные методы-формирователи тестовых случаев ValuePropertyGoodValuesTestCases, ValuePropertyBadValuesTestCases, GetImpedanceMethodBadFrequenciesTestCases, ConstructorNoParametersTestCase; эти методы возвращают значения одноименных статических методов класса RadioComponentTests<Resistor>
    - добавлены и реализованы публичные тестовые методы ValueProperty_AssignedGoodValue_IsAssigned, ValueProperty_AssignedBadValue_ThrowsExpectedException, GetImpedance_BadFrequency_ThrowsExpectedException, GetImpedance_GoodParametersAssigned_ReturnsExpectedImpedance, UnitTypeQuantityProperties_Always_ReturnsValues, Constructor_NoParameters_SetsDefaultRadioComponentValue; эти методы вызывают одноименные методы класса RadioComponentTests<Resistor> через объект _radioComponentTests
    - перед всеми тестовыми методами добавлены соответствующие им атрибуты TestCaseSource и TestCase (добавлены тестовые случаи)

--- Testing/Model.UnitTests/InductorTests.cs
+++ Testing/Model.UnitTests/InductorTests.cs
@@ -11,14 +11,49 @@ namespace Model.UnitTests
 	/// Набор тестов для класса <see cref="Inductor"/>
 	/// </summary>
 	[TestFixture]
-	class InductorTests : RadioComponentTests<Inductor>
+	class InductorTests
 	{
+		private RadioComponentTests<Inductor> _radioComponentTests
+			= new RadioComponentTests<Inductor>();
+
+		// TestCaseData
+
+		public static
+			IEnumerable<TestCaseData> ValuePropertyGoodValuesTestCases()
+		{
+			return RadioComponentTests<Inductor>
+				.ValuePropertyGoodValuesTestCases();
+		}
+
+		public static
+			IEnumerable<TestCaseData> ValuePropertyBadValuesTestCases()
+		{
+			return RadioComponentTests<Inductor>
+				.ValuePropertyBadValuesTestCases();
+		}
+
+		public static IEnumerable<TestCaseData>
+			GetImpedanceMethodBadFrequenciesTestCases()
+		{
+			return RadioComponentTests<Inductor>
+				.GetImpedanceMethodBadFrequenciesTestCases();
+		}
+
+		public static
+			IEnumerable<TestCaseData> ConstructorNoParametersTestCase()
+		{
+			return RadioComponentTests<Inductor>
+				.ConstructorNoParametersTestCase();
+		}
+
 		private static
 			IEnumerable<TestCaseData> GetImpedanceMethodTestCases()
 		{
-			foreach (var radioComponentValue in GoodRadioComponentValues)
+			foreach (var radioComponentValue in
+				RadioComponentTests<Inductor>.GoodRadioComponentValues)
 			{
-				foreach (var frequency in GoodFrequencies)
+				foreach (var frequency in
+					RadioComponentTests<Inductor>.GoodFrequencies)
 				{
 					var expectedImpedance
 						= new Complex(0,
@@ -35,14 +70,60 @@ namespace Model.UnitTests
 			}
 		}
 
-		[Test, TestCaseSource("GetImpedanceMethodTestCases")]
-		public override void
+		// Tests
+
+		[TestCaseSource("ValuePropertyGoodValuesTestCases")]
+		public void ValueProperty_AssignedGoodValue_IsAssigned(double value)
+		{
+			_radioComponentTests
+				.ValueProperty_AssignedGoodValue_IsAssigned(value);
+		}
+
+		[TestCaseSource("ValuePropertyBadValuesTestCases")]
+		public void ValueProperty_AssignedBadValue_ThrowsExpectedException(
+			KeyValuePair<double, Type> doubleToExpectedExceptionType)
+		{
+			_radioComponentTests
+				.ValueProperty_AssignedBadValue_ThrowsExpectedException(
+					doubleToExpectedExceptionType);
+		}
+
+		[TestCaseSource("GetImpedanceMethodBadFrequenciesTestCases")]
+		public void GetImpedance_BadFrequency_ThrowsExpectedException(
+			KeyValuePair<double, Type> doubleToExpectedExceptionType)
+		{
+			_radioComponentTests
+				.GetImpedance_BadFrequency_ThrowsExpectedException(
+					doubleToExpectedExceptionType);
+		}
+
+		[TestCaseSource("ConstructorNoParametersTestCase")]
+		public void Constructor_NoParameters_SetsDefaultRadioComponentValue()
+		{
+			_radioComponentTests
+				.Constructor_NoParameters_SetsDefaultRadioComponentValue();
+		}
+
+		[TestCaseSource("GetImpedanceMethodTestCases")]
+		public void
 			GetImpedance_GoodParametersAssigned_ReturnsExpectedImpedance(
 				double frequency, double radioComponentValue,
 				Complex expectedImpedance)
 		{
-			base.GetImpedance_GoodParametersAssigned_ReturnsExpectedImpedance(
-				frequency, radioComponentValue, expectedImpedance);
+			_radioComponentTests
+				.GetImpedance_GoodParametersAssigned_ReturnsExpectedImpedance(
+					frequency, radioComponentValue, expectedImpedance);
+		}
+
+		[TestCase("Гн", "Катушка индуктивности", "Индуктивность",
+			TestName = "Когда вызываются свойства Unit, Type, Quantity, " +
+			"то они должны вернуть значения Гн, Катушка индуктивности, " +
+			"Индуктивность соответственно.")]
+		public void UnitTypeQuantityProperties_Always_ReturnsValues(
+			params string[] expectedValues)
+		{
+			_radioComponentTests.UnitTypeQuantityProperties_Always_ReturnsValues(
+				expectedValues);
 		}
 	}
 }
