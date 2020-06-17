abc3daab41374217540efdf958d8d1c53c5de260
TemZZZ <temsky.v.tomske@gmail.com>
2020-06-17 19:05:50 +0700

    feature(Model.UnitTests): добавлен тестовый класс RadioComponentFactoryTests
    - добавлен и реализован метод CreateRadioComponent_AssignedGoodValues_ReturnsObjectOfExpectedType
    - добавлены тесты для тестирования метода CreateRadioComponent (атрибуты перед методом CreateRadioComponent_AssignedGoodValues_ReturnsObjectOfExpectedType, тесты пока без имен)

---
+++ Testing/Model.UnitTests/RadioComponentFactoryTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+using Model.PassiveComponents;
+
+
+namespace Model.UnitTests
+{
+	[TestFixture]
+	class RadioComponentFactoryTests
+	{
+		// TODO Захерачить имена тестам!
+		[TestCase(RadioComponentType.Resistor, 500, typeof(Resistor))]
+		[TestCase(RadioComponentType.Inductor, 500, typeof(Inductor))]
+		[TestCase(RadioComponentType.Capacitor, 500, typeof(Capacitor))]
+		public void
+			CreateRadioComponent_AssignedGoodValues_ReturnsObjectOfExpectedType(
+				RadioComponentType radioComponentType,
+				double radioComponentValue, Type expectedType)
+		{
+			// Arrange
+			var radioComponentFactory = new RadioComponentFactory();
+
+			// Act
+			object actualObject = radioComponentFactory.CreateRadioComponent(
+				radioComponentType, radioComponentValue);
+
+			// Assert
+			Assert.AreEqual(actualObject.GetType(), expectedType);
+		}
+	}
+}
