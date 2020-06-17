a7f3af0fa4734597f62b7641727d7f96fb33c5b9
TemZZZ <temsky.v.tomske@gmail.com>
2020-06-17 16:59:58 +0700

    feature(Model): добавлен и реализован класс RadioComponentFactory
    - реализован метод CreateRadioComponent, возвращающий радиокомпонент нужного типа

---
+++ Model/RadioComponentFactory.cs
@@ -0,0 +1,56 @@
+﻿using Model.PassiveComponents;
+using System.Collections.Generic;
+
+
+namespace Model
+{
+	/// <summary>
+	/// Класс фабрики радиокомпонентов
+	/// </summary>
+	public class RadioComponentFactory
+	{
+		/// <summary>
+		/// Перечислитель пар "тип радиокомпонента-радиокомпонент"
+		/// </summary>
+		/// <param name="radioComponentValue">Значение физической величины
+		/// радиокомпонента</param>
+		/// <returns>Пара значений "тип радиокомпонента-радиокомпонент"
+		/// </returns>
+		private IEnumerable<(RadioComponentType radioComponentType,
+			RadioComponentBase radioComponent)>
+				TypeToRadioComponentMap(double radioComponentValue)
+		{
+			yield return (RadioComponentType.Resistor,
+				new Resistor(radioComponentValue));
+			yield return (RadioComponentType.Inductor,
+				new Inductor(radioComponentValue));
+			yield return (RadioComponentType.Capacitor,
+				new Capacitor(radioComponentValue));
+		}
+
+		/// <summary>
+		/// Возвращает радиокомпонент определенного типа с требуемым
+		/// значением физической величины
+		/// </summary>
+		/// <param name="radioComponentType">Тип радиокомпонента</param>
+		/// <param name="radioComponentValue">Значение физической величины
+		/// радиокомпонента</param>
+		/// <returns>Объект класса-наследника
+		/// <see cref="RadioComponentBase"/></returns>
+		public RadioComponentBase CreateRadioComponent(
+			RadioComponentType radioComponentType,
+			double radioComponentValue)
+		{
+			foreach (var (type, radioComponent)
+				in TypeToRadioComponentMap(radioComponentValue))
+			{
+				if (radioComponentType == type)
+				{
+					return radioComponent;
+				}
+			}
+
+			return null;
+		}
+	}
+}
