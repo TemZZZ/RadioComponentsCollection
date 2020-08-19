c69cc76e951732897296a1cd95e7724c00407677
TemZZZ <temsky.v.tomske@gmail.com>
2020-08-19 23:40:26 +0700

    feature(project Model, RadiocomponentBase): реализованы свойства Quantity и Unit

---
+++ Model/RadiocomponentBase.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Numerics;
+using System.Xml.Serialization;
+using Model.PassiveComponents;
+
+
+namespace Model
+{
+	/// <summary>
+	/// Абстрактный класс радиокомпонента.
+	/// Его наследуют все производные классы радиокомпонентов
+	/// </summary>
+	[XmlInclude(typeof(Resistor))]
+	[XmlInclude(typeof(Inductor))]
+	[XmlInclude(typeof(Capacitor))]
+	public abstract class RadiocomponentBase : IRadiocomponent
+	{
+        private const double _defaultValue = 0;
+		/// <summary>
+		/// Хранит значение физической величины радиокомпонента
+		/// </summary>
+		private double _value;
+
+		/// <summary>
+		/// Проверяет именованый параметр вещественного типа на
+		/// принадлежность допустимому диапазону значений
+		/// </summary>
+		/// <param name="parameter">Параметр</param>
+		/// <param name="parameterName">Имя параметра</param>
+		/// <exception cref="ArgumentOutOfRangeException">Выбрасывается при
+		/// попытке присвоения отрицательного или бесконечно большого
+		/// положительного значения</exception>
+		/// <exception cref="ArgumentException"> Выбрасывается при попытке
+		/// присвоения NaN</exception>
+		private void CheckNamedDoubleParameter(double parameter,
+			string parameterName)
+		{
+			if (double.IsNaN(parameter))
+			{
+				throw new ArgumentException(
+					$"{parameterName} can't be NaN.");
+			}
+
+			if (double.IsPositiveInfinity(parameter))
+			{
+				throw new ArgumentOutOfRangeException(
+					$"{parameterName} must be less than or equal to " +
+					$"{double.MaxValue}.");
+			}
+
+			if (parameter < 0)
+			{
+				throw new ArgumentOutOfRangeException(
+					$"{parameterName} can't be less than zero.");
+			}
+		}
+
+		/// <summary>
+		/// Создает экземпляр класса <see cref="RadiocomponentBase"/>
+		/// </summary>
+		/// <param name="value">Значение физической величины в СИ</param>
+		protected RadiocomponentBase(double value)
+		{
+			Value = value;
+		}
+
+        /// <summary>
+        /// Этот конструктор вызывается при создании экземпляров
+        /// классов-наследников <see cref="RadiocomponentBase"/>
+        /// </summary>
+        protected RadiocomponentBase() : this(_defaultValue) { }
+
+		/// <summary>
+		/// Позволяет получить или присвоить значение
+		/// физической величины радиокомпонента
+		/// </summary>
+		public double Value
+		{
+            get => _value;
+            set
+            {
+                const string parameterName
+                    = "physical value of radiocomponent";
+                RadiocomponentService.ValidatePositiveDouble(
+                    value, parameterName);
+                _value = value;
+            }
+		}
+
+		/// <summary>
+		/// Возвращает частотнозависимый комплексный
+		/// импеданс радиокомпонента
+		/// </summary>
+		public Complex GetImpedance(double frequency)
+		{
+            RadiocomponentService.ValidatePositiveDouble(
+                frequency, nameof(frequency));
+            return CalculateImpedance(frequency);
+		}
+
+		/// <summary>
+		/// Возвращает строковое представление объекта
+		/// </summary>
+		public override string ToString()
+		{
+            return RadiocomponentService.ToString(Type, Value);
+		}
+
+        /// <inheritdoc />
+        public RadiocomponentQuantity Quantity
+            => RadiocomponentService.GetRadiocomponentQuantity(Type);
+
+        /// <inheritdoc />
+        public RadiocomponentUnit Unit
+            => RadiocomponentService.GetRadiocomponentUnit(Type);
+
+		/// <summary>
+		/// Вычисляет частотнозависимый комплексный импеданс
+		/// и возвращает значение
+		/// </summary>
+		/// <param name="frequency">Частота в герцах</param>
+		/// <returns>Комплексный импеданс в омах</returns>
+		protected abstract Complex CalculateImpedance(double frequency);
+
+		/// <inheritdoc/>
+		public abstract RadioComponentType Type { get; }
+	}
+}
