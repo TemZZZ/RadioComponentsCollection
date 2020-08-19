7069b8ebdf72c8c78183d131beabc29f6d2470a3
TemZZZ <temsky.v.tomske@gmail.com>
2020-08-19 22:24:35 +0700

    feature(project Model): добавлен класс RadiocomponentService
    - в классе реализован словарь _radiocomponentTypeToPropertiesMap, ставящий в соответствие типам радиокомпонентов физ. величины и единицы измерения

---
+++ Model/RadiocomponentService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    /// <summary>
+    /// Сервисный класс со вспомогательными функциями для работы с
+    /// радиокомпонентами
+    /// </summary>
+    public static class RadiocomponentService
+    {
+        /// <summary>
+        /// Словарь, ставящий в соответствие типам радиокомпонентов их
+        /// физические величины и единицы измерения
+        /// </summary>
+        private static readonly
+            Dictionary<RadioComponentType,
+                (RadiocomponentQuantity Quantity, RadiocomponentUnit Unit)>
+            _radiocomponentTypeToPropertiesMap
+                = new Dictionary<RadioComponentType,
+                    (RadiocomponentQuantity, RadiocomponentUnit)>
+                {
+                    [RadioComponentType.Resistor]
+                        = (RadiocomponentQuantity.Resistance,
+                            RadiocomponentUnit.Ohm),
+                    [RadioComponentType.Inductor]
+                        = (RadiocomponentQuantity.Inductance,
+                            RadiocomponentUnit.Henry),
+                    [RadioComponentType.Capacitor]
+                        = (RadiocomponentQuantity.Capacitance,
+                            RadiocomponentUnit.Farad)
+                };
+    }
+}
