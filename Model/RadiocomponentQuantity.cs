078f58823f7675e3a8fb1fd1359485d93651a513
TemZZZ <temsky.v.tomske@gmail.com>
2020-08-19 22:04:19 +0700

    feature(project Model): добавлен перечислимый тип RadiocomponentQuantity, предстваляющий физические величины радиокомпонентов

---
+++ Model/RadiocomponentQuantity.cs
@@ -0,0 +1,12 @@
+﻿namespace Model
+{
+    /// <summary>
+    /// Физическая величина радиокомпонента
+    /// </summary>
+    public enum RadiocomponentQuantity
+    {
+        Resistance,
+        Inductance,
+        Capacitance
+    }
+}
