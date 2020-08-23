6f24c7de9346cd47fbda18a3e3536ac4b070a775
TemZZZ <temsky.v.tomske@gmail.com>
2020-08-23 12:19:59 +0700

    feature(project Model.UnitTests): добавлен класс RadiocomponentServiceTests
    - добавлен и реализован генератор негативных тестовых случаев ToRadiocomponentTypeBadParameterTestCasesData для метода RadiocomponentService.ToRadiocomponentType

---
+++ Testing/Model.UnitTests/RadiocomponentServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Model.UnitTests
+{
+    [TestFixture]
+    class RadiocomponentServiceTests
+    {
+        #region -- TestCasesData --
+
+        public static IEnumerable<TestCaseData>
+            ToRadiocomponentTypeBadParameterTestCasesData()
+        {
+            string[] badStrings =
+            {
+                null,
+                string.Empty,
+                "Some wrong string"
+            };
+
+            foreach (var badString in badStrings)
+            {
+                yield return new TestCaseData(badString,
+                    typeof(ArgumentException)).SetName(
+                    "Когда метод " +
+                    $"{nameof(RadiocomponentService.ToRadiocomponentType)} " +
+                    $"вызывается с параметром {badString}, то должно " +
+                    "выбрасываться исключение " +
+                    $"{typeof(ArgumentException).Name}.");
+            }
+        }
+
+        #endregion
+    }
+}
