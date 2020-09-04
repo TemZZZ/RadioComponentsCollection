1233d36e318a0afe9a9908d3749870b56cd7227a
TemZZZ <temsky.v.tomske@gmail.com>
2020-09-04 11:27:25 +0700

    feature(project MVVM): добавлен класс AddRadiocomponentViewModel
    - реализован список радиокомпонентов _radiocomponentTypes, которые можно будет создавать
    - реализован метод GetRadiocomponentTypeAsStringToQuantityUnitAsStringMap
    - реализовано свойство RadiocomponentTypeAsStringToQuantityUnitAsStringMap

---
+++ MVVM/AddRadiocomponentViewModel.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace MVVM
+{
+    class AddRadiocomponentViewModel : ViewModelBase
+    {
+        #region -- Private fields --
+
+        /// <summary>
+        /// Типы радиокомпонентов, которые можно будет создавать.
+        /// </summary>
+        private readonly RadiocomponentType[] _radiocomponentTypes =
+        {
+            RadiocomponentType.Resistor,
+            RadiocomponentType.Inductor,
+            RadiocomponentType.Capacitor
+        };
+
+        #endregion
+
+        #region -- Private methods --
+
+        /// <summary>
+        /// Формирует и возвращает ассоциативный массив, ставящий в
+        /// соответствие строковому представлению типа радиокомпонента
+        /// строковые представления его физической величины и единицы
+        /// измерения.
+        /// </summary>
+        /// <param name="radiocomponentTypes">Типы радиокомпонентов.</param>
+        /// <returns>Ассоциативный массив, ставящий в соответствие строковому
+        /// представлению типа радиокомпонента строковые представления его
+        /// физической величины и единицы измерения.</returns>
+        private List<(string, string)>
+            GetRadiocomponentTypeAsStringToQuantityUnitAsStringMap(
+                IEnumerable<RadiocomponentType> radiocomponentTypes)
+        {
+            var typeAsStringToQuantityUnitAsStringMap
+                = new List<(string, string)>();
+
+            foreach (var radiocomponentType in radiocomponentTypes)
+            {
+                var radiocomponentTypeAsString = RadiocomponentService
+                    .ToString(radiocomponentType);
+
+                var radiocomponentQuantity = RadiocomponentService
+                    .GetRadiocomponentQuantity(radiocomponentType);
+                var radiocomponentQuantityAsString = RadiocomponentService
+                    .ToString(radiocomponentQuantity);
+
+                var radiocomponentUnit = RadiocomponentService
+                    .GetRadiocomponentUnit(radiocomponentType);
+                var radiocomponentUnitAsString = RadiocomponentService
+                    .ToString(radiocomponentType);
+
+                var quantityUnitAsString
+                    = radiocomponentQuantityAsString
+                      + ", " + radiocomponentUnitAsString;
+
+                typeAsStringToQuantityUnitAsStringMap.Add(
+                    (radiocomponentTypeAsString, quantityUnitAsString));
+            }
+
+            return typeAsStringToQuantityUnitAsStringMap;
+        }
+
+        #endregion
+
+        #region -- Public properties --
+
+        /// <summary>
+        /// Позволяет получить ассоциативный массив, ставящий в соответствие
+        /// строковому представлению типа радиокомпонента строковые
+        /// представления его физической величины и единицы измерения.
+        /// </summary>
+        public List<(string, string)>
+            RadiocomponentTypeAsStringToQuantityUnitAsStringMap
+                => GetRadiocomponentTypeAsStringToQuantityUnitAsStringMap(
+                    _radiocomponentTypes);
+
+        #endregion
+    }
+}
