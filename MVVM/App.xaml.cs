dcc7122eca01ad464797cb3663ad40dbef74fb0a
TemZZZ <temsky.v.tomske@gmail.com>
2020-09-07 18:04:46 +0700

    feature(project MVVM, App.xaml.cs): создан экземпляр класса PresentationRootRegistry; зарегистрированы типы вьюмоделей и представлений; создается главное окно и главная вьюмодель при запуске приложения

--- MVVM/App.xaml.cs
+++ MVVM/App.xaml.cs
@@ -13,13 +13,37 @@ namespace MVVM
     /// </summary>
     public partial class App : Application
     {
+        private readonly PresentationRootRegistry
+            _presentationRootRegistry = new PresentationRootRegistry();
+
+        /// <summary>
+        /// Регистрирует типы представлений на типы моделей представлений.
+        /// </summary>
+        private void RegisterWindowsTypes()
+        {
+            _presentationRootRegistry.RegisterWindowType
+                <MainViewModel, MainWindow>();
+            _presentationRootRegistry.RegisterWindowType
+                <AddRadiocomponentViewModel, AddRadiocomponentView>();
+        }
+
         static App()
         {
             // Добавлено для возможности ввода десятичной точки в элемент
             // управления TextBox, когда свойство Text последнего привязано
-            // к свойству зависимости типа double или decimal
+            // к свойству зависимости типа double или decimal.
             FrameworkCompatibilityPreferences
                 .KeepTextBoxDisplaySynchronizedWithTextProperty = false;
+        }
+
+        public App()
+        {
+            RegisterWindowsTypes();
+
+            var mainViewModel = new MainViewModel(_presentationRootRegistry);
+            var mainWindow = _presentationRootRegistry
+                .CreateWindowWithDataContext(mainViewModel);
+            mainWindow.Show();
         }
     }
 }
