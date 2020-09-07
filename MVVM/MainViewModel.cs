b565dd3daf4e0f4edbc7cf5889b992580b039ec0
TemZZZ <temsky.v.tomske@gmail.com>
2020-09-07 18:30:24 +0700

    feature(project MVVM, MainViewModel): добавлена команда OpenAddRadiocomponentWindow; команда привязана к кнопке Добавить в XAML файле

--- MVVM/MainViewModel.cs
+++ MVVM/MainViewModel.cs
@@ -3,16 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace MVVM
 {
     internal class MainViewModel : ViewModelBase
     {
+        #region -- Private fields --
+
         private readonly PresentationRootRegistry _presentationRootRegistry;
+        private RelayCommand _openAddRadiocomponentWindow;
+
+        #endregion
+
+        #region -- Constructors --
+
         public MainViewModel(
             PresentationRootRegistry presentationRootRegistry)
         {
             _presentationRootRegistry = presentationRootRegistry;
         }
+
+        #endregion
+
+        #region -- Commands --
+
+        public RelayCommand OpenAddRadiocomponentWindow
+            => _openAddRadiocomponentWindow ?? (_openAddRadiocomponentWindow
+                = new RelayCommand(
+                    obj =>
+                    {
+                        var addRadiocomponentViewModel
+                            = new AddRadiocomponentViewModel();
+                        var addRadiocomponentWindow = _presentationRootRegistry
+                            .CreateWindowWithDataContext(addRadiocomponentViewModel);
+                        addRadiocomponentWindow.WindowStartupLocation
+                            = WindowStartupLocation.CenterScreen;
+                        addRadiocomponentWindow.ShowDialog();
+                    }));
+
+        #endregion
     }
 }
