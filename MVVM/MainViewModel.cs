0066bca00d1f68eeb5186bc18de4e28e6b81a5cc
TemZZZ <temsky.v.tomske@gmail.com>
2020-09-07 17:45:05 +0700

    feature(project MVVM): добавлен класс MainViewModel
    - в конструктор класса передается экземпляр класса PresentationRootRegistry

---
+++ MVVM/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM
+{
+    internal class MainViewModel : ViewModelBase
+    {
+        private readonly PresentationRootRegistry _presentationRootRegistry;
+        public MainViewModel(
+            PresentationRootRegistry presentationRootRegistry)
+        {
+            _presentationRootRegistry = presentationRootRegistry;
+        }
+    }
+}
