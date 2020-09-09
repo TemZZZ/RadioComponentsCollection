b917d08eb74336635bc7e8097112e7f46d6c140a
TemZZZ <temsky.v.tomske@gmail.com>
2020-09-09 11:44:25 +0700

    feature(project Model): добавлен и реализован класс адаптера радиокомпонента к его удобочитаемому представлению RadiocomponentToPrintableRadiocomponentAdapter

---
+++ Model/RadiocomponentToPrintableRadiocomponentAdapter.cs
@@ -0,0 +1,50 @@
+﻿namespace Model
+{
+    /// <summary>
+    /// Класс адаптера радиокомпонента к его удобочитаемому представлению.
+    /// </summary>
+    public class RadiocomponentToPrintableRadiocomponentAdapter
+        : IPrintableRadiocomponent
+    {
+        private RadiocomponentBase _radiocomponent;
+
+        /// <summary>
+        /// Создает объект адаптера радиокомпонента к его удобочитаемому
+        /// представлению.
+        /// </summary>
+        /// <param name="radiocomponent">Адаптируемый радиокомпонент.</param>
+        public RadiocomponentToPrintableRadiocomponentAdapter(
+            RadiocomponentBase radiocomponent)
+        {
+            _radiocomponent = radiocomponent;
+        }
+
+        /// <inheritdoc/>
+        public string Type
+            => RadiocomponentService.ToString(_radiocomponent.Type);
+
+        /// <inheritdoc/>
+        public string Quantity
+            => RadiocomponentService.ToString(_radiocomponent.Quantity);
+
+        /// <inheritdoc/>
+        public double Value
+        {
+            get => _radiocomponent.Value;
+            set => _radiocomponent.Value = value;
+        }
+
+        /// <inheritdoc/>
+        public string Unit
+            => RadiocomponentService.ToString(_radiocomponent.Unit);
+
+        /// <summary>
+        /// Позволяет получить адаптируемый радиокомпонент.
+        /// </summary>
+        /// <returns>Адаптируемый радиокомпонент.</returns>
+        public RadiocomponentBase GetRadiocomponent()
+        {
+            return _radiocomponent;
+        }
+    }
+}
