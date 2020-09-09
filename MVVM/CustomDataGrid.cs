e4313e9ca28dfc51712b139e182223ef9764e2e3
TemZZZ <temsky.v.tomske@gmail.com>
2020-09-09 12:53:54 +0700

    feature(project MVVM): добавлен и реализован класс таблицы с возможностью привязки к выделенным элементам CustomDataGrid

---
+++ MVVM/CustomDataGrid.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MVVM
+{
+    /// <summary>
+    /// Класс элемента управления содержимым, представляющий таблицу
+    /// <see cref="DataGrid"/>, но имеющий возможность привязки к выделенным
+    /// элементам, чего нет в обычном <see cref="DataGrid"/>.
+    /// </summary>
+    public class CustomDataGrid : DataGrid
+    {
+        #region -- Dependency properties --
+
+        /// <summary>
+        /// Свойство зависимости, к которому можно привязаться,
+        /// представляющее выделенные элементы таблицы.
+        /// </summary>
+        public static readonly
+            DependencyProperty BindableSelectedItemsProperty;
+
+        #endregion
+
+        #region -- Constructors --
+
+        /// <summary>
+        /// Регистрирует свойства зависимостей.
+        /// </summary>
+        static CustomDataGrid()
+        {
+            BindableSelectedItemsProperty = DependencyProperty.Register(
+                nameof(BindableSelectedItems), typeof(IList),
+                typeof(CustomDataGrid), new PropertyMetadata(null));
+        }
+
+        /// <summary>
+        /// Создает экземпляр класса <see cref="CustomDataGrid"/>.
+        /// </summary>
+        public CustomDataGrid()
+        {
+            SelectionChanged += OnCustomDataGridSelectionChanged;
+        }
+
+        #endregion
+
+        #region -- Public properties --
+
+        /// <summary>
+        /// Позволяет получить или задать выделенные элементы таблицы.
+        /// </summary>
+        public IList BindableSelectedItems
+        {
+            get => (IList)GetValue(BindableSelectedItemsProperty);
+            set => SetValue(BindableSelectedItemsProperty, value);
+        }
+
+        #endregion
+
+        #region -- Auxiliary private methods --
+
+        /// <summary>
+        /// Позволяет получить или задать выделенные элементы таблицы.
+        /// </summary>
+        /// <summary>
+        /// Срабатывает при изменении выделения таблицы. Присваивает свойству
+        /// <see cref="BindableSelectedItems"/> выделенные элементы.
+        /// </summary>
+        private void OnCustomDataGridSelectionChanged(object sender,
+            SelectionChangedEventArgs e)
+        {
+            BindableSelectedItems = SelectedItems;
+        }
+
+        #endregion
+    }
+}
