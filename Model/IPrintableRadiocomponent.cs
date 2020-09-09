ad86e28104a73d72273ba16ab91bb0a37f747d3e
TemZZZ <temsky.v.tomske@gmail.com>
2020-09-09 11:43:35 +0700

    feature(project Model): добавлен интерфейс удобочитаемого представления радиокомпонента IPrintableRadiocomponent

---
+++ Model/IPrintableRadiocomponent.cs
@@ -0,0 +1,30 @@
+﻿namespace Model
+{
+    /// <summary>
+    /// Интерфейс удобочитаемого представления радиокомпонента.
+    /// </summary>
+    public interface IPrintableRadiocomponent
+    {
+        /// <summary>
+        /// Позволяет получить тип радиокомпонента.
+        /// </summary>
+        string Type { get; }
+
+        /// <summary>
+        /// Позволяет получить физическую величину радиокомпонента.
+        /// </summary>
+        string Quantity { get; }
+
+        /// <summary>
+        /// Позволяет получить или задать значение физической величины
+        /// радиокомпонента.
+        /// </summary>
+        double Value { get; set; }
+
+        /// <summary>
+        /// Позволяет получить единицу измерения физической величины
+        /// радиокомпонента.
+        /// </summary>
+        string Unit { get; }
+    }
+}
