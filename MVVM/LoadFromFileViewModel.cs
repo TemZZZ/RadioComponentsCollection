a7b1beeb6ce59c1a2aecc4960f0d03d13abb696f
TemZZZ <temsky.v.tomske@gmail.com>
2020-09-11 10:59:10 +0700

    feature(project MVVM): добавлены вьюмодели
    - LoadFromFileViewModel с реализованными свойствами WindowTitle и ActionName
    - SaveToFileViewModel с реализованными свойствами WindowTitle и ActionName

---
+++ MVVM/LoadFromFileViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace MVVM
+{
+    internal class LoadFromFileViewModel : ViewModelBase, IActionViewModel
+    {
+        public string WindowTitle => "Загрузить радиокомпоненты из файла";
+        public List<(string, string)> Options { get; }
+        public string ActionName => "Загрузить";
+        public RelayCommand ActionCommand { get; }
+    }
+}
---
+++ MVVM/SaveToFileViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace MVVM
+{
+    internal class SaveToFileViewModel : ViewModelBase, IActionViewModel
+    {
+        public string WindowTitle => "Сохранить радиокомпоненты в файл";
+        public List<(string, string)> Options { get; }
+        public string ActionName => "Сохранить";
+        public RelayCommand ActionCommand { get; }
+    }
+}
