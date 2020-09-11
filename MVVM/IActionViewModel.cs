82fb1395516ed531776d1ff4f4ae78c3ac77d0c1
TemZZZ <temsky.v.tomske@gmail.com>
2020-09-11 10:47:11 +0700

    feature(project MVVM): добавлен интерфейс модели представления действия с опциями IActionViewModel

---
+++ MVVM/IActionViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace MVVM
+{
+    /// <summary>
+    /// Интерфейс модели представления действия с опциями.
+    /// </summary>
+    interface IActionViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Заголовок окна.
+        /// </summary>
+        string WindowTitle { get; }
+
+        /// <summary>
+        /// Дополнительные опции для действия. Нужно для отображения
+        /// коллекции радиокнопок с именами опций. Вторые части кортежей
+        /// могут быть любыми и не участвуют в формировании разметки.
+        /// </summary>
+        List<(string, string)> Options { get; }
+
+        /// <summary>
+        /// Имя действия.
+        /// </summary>
+        string ActionName { get; }
+
+        /// <summary>
+        /// Команда, выполняющая действие.
+        /// </summary>
+        RelayCommand ActionCommand { get; }
+    }
+}
