ea6d7dc81e3f8d81e136ba0e2a35b1b047d4db71
TemZZZ <temsky.v.tomske@gmail.com>
2020-09-11 15:07:51 +0700

    feature(project MVVM, SaveToFileViewModel): добавлена возможность сохранять радиокомпоненты в файл в команде ActionCommand
    - добавлены поля _radiocomponents и _selectedRadiocomponents, хранящие выделенные и все радиокомпоненты соответственно
    - добавлен и реализован метод GetWritingRadiocomponents, возвращающий сохраняемые радиокомпоненты
    - в команде ActionCommand реализовано сохранение либо всех, либо выделенных радиокомпонентов в зависимости от выбранной опции

--- MVVM/SaveToFileViewModel.cs
+++ MVVM/SaveToFileViewModel.cs
@@ -18,14 +18,39 @@ namespace MVVM
                         = "Сохранить только выделенные радиокомпоненты"
                 };
 
+        private IEnumerable<IPrintableRadiocomponent> _radiocomponents;
+        private IList _selectedRadiocomponents;
+
         private uint? _selectedOptionIndex;
         private RelayCommand _openLoadFromFileDialogCommand;
+
+        /// <summary>
+        /// Возвращает список сохраняемых радиокомпонентов.
+        /// </summary>
+        /// <param name="printableRadiocomponents">Адаптированные
+        /// удобочитаемые радиокомпоненты.</param>
+        /// <returns>Список сохраняемых радиокомпонентов.</returns>
+        private List<RadiocomponentBase> GetWritingRadiocomponents(
+            IEnumerable printableRadiocomponents)
+        {
+            var writingRadiocomponents = new List<RadiocomponentBase>();
+            foreach (var radiocomponentAsObject in printableRadiocomponents)
+            {
+                var printableRadiocomponent
+                    = (IPrintableRadiocomponent)radiocomponentAsObject;
+                var radicomponent
+                    = printableRadiocomponent.GetRadiocomponent();
+                writingRadiocomponents.Add(radicomponent);
+            }
+            return writingRadiocomponents;
+        }
 
         public SaveToFileViewModel(
             IEnumerable<IPrintableRadiocomponent> radiocomponents,
             IList selectedRadiocomponents)
         {
-            
+            _radiocomponents = radiocomponents;
+            _selectedRadiocomponents = selectedRadiocomponents;
         }
 
         public string WindowTitle => "Сохранить радиокомпоненты в файл";
@@ -52,7 +77,47 @@ namespace MVVM
                ?? (_openLoadFromFileDialogCommand
                    = new RelayCommand(obj =>
                    {
+                       IDIalogService saveFileDialog
+                           = new DefaultDialogService();
 
+                       var option = _saveOptionToOptionDescriptionMap
+                           .Keys.ElementAt((int)SelectedOptionIndex);
+                       var writingRadiocomponents
+                           = new List<RadiocomponentBase>();
+                       switch (option)
+                       {
+                           case RadiocomponentsSaveOption.SaveAll:
+                               writingRadiocomponents
+                                   = GetWritingRadiocomponents(
+                                       _radiocomponents);
+                               break;
+                           case RadiocomponentsSaveOption.SaveSelected:
+                               writingRadiocomponents
+                                   = GetWritingRadiocomponents(
+                                       _selectedRadiocomponents);
+                               break;
+                       }
+
+                       if (writingRadiocomponents.Count == 0)
+                       {
+                           saveFileDialog.ShowMessage(
+                               "Не выделено ни одного радиокомпонента для " +
+                               "сохранения.");
+                           return;
+                       }
+
+                       if (!saveFileDialog.SaveFileDialog())
+                       {
+                           return;
+                       }
+
+                       if (saveFileDialog.FilePath != null)
+                       {
+                           var xmlWriter = new XmlReaderWriter();
+                           xmlWriter.SerializeAndWriteXml(
+                               writingRadiocomponents,
+                               saveFileDialog.FilePath);
+                       }
                    }, obj => SelectedOptionIndex != null));
     }
 }
