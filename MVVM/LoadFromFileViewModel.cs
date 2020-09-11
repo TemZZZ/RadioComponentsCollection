e089492028f57cbba71cdbc833e597dac7017ee3
TemZZZ <temsky.v.tomske@gmail.com>
2020-09-11 11:54:01 +0700

    feature(project MVVM, SaveToFileViewModel, LoadFromFileViewModel): добавлены доступные опции

--- MVVM/LoadFromFileViewModel.cs
+++ MVVM/LoadFromFileViewModel.cs
@@ -1,11 +1,28 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using View;
 
 namespace MVVM
 {
     internal class LoadFromFileViewModel : ViewModelBase, IActionViewModel
     {
+        private readonly Dictionary<RadiocomponentsLoadOption, string>
+            _loadOptionToOptionDescriptionMap
+                = new Dictionary<RadiocomponentsLoadOption, string>
+                {
+                    [RadiocomponentsLoadOption.AddToEnd]
+                        = "Добавить в конец таблицы",
+                    [RadiocomponentsLoadOption.ReplaceAll]
+                        = "Заменить все радиокомпоненты в таблице новыми"
+                };
+
         public string WindowTitle => "Загрузить радиокомпоненты из файла";
-        public List<(string, string)> Options { get; }
+
+        public List<(string, string)> Options
+            => _loadOptionToOptionDescriptionMap.Values
+                .Select(optionDescription
+                    => ((string, string))(optionDescription, null)).ToList();
+
         public string ActionName => "Загрузить";
         public RelayCommand ActionCommand { get; }
     }
--- MVVM/SaveToFileViewModel.cs
+++ MVVM/SaveToFileViewModel.cs
@@ -1,11 +1,28 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using View;
 
 namespace MVVM
 {
     internal class SaveToFileViewModel : ViewModelBase, IActionViewModel
     {
+        private readonly Dictionary<RadiocomponentsSaveOption, string>
+            _saveOptionToOptionDescriptionMap
+                = new Dictionary<RadiocomponentsSaveOption, string>
+                {
+                    [RadiocomponentsSaveOption.SaveAll]
+                        = "Сохранить все радиокомпоненты",
+                    [RadiocomponentsSaveOption.SaveSelected]
+                        = "Сохранить только выделенные радиокомпоненты"
+                };
+
         public string WindowTitle => "Сохранить радиокомпоненты в файл";
-        public List<(string, string)> Options { get; }
+
+        public List<(string, string)> Options
+            => _saveOptionToOptionDescriptionMap.Values
+                .Select(optionDescription
+                    => ((string, string))(optionDescription, null)).ToList();
+
         public string ActionName => "Сохранить";
         public RelayCommand ActionCommand { get; }
     }
