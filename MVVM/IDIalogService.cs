e71cce71bd150c000fd663d1adf56272bd5ed30a
TemZZZ <temsky.v.tomske@gmail.com>
2020-09-11 12:59:41 +0700

    feature(project MVVM): добавлен интерфейс IDIalogService

---
+++ MVVM/IDIalogService.cs
@@ -0,0 +1,34 @@
+﻿namespace MVVM
+{
+    public interface IDIalogService
+    {
+        /// <summary>
+        /// Показ сообщения в диалоговом окне.
+        /// </summary>
+        /// <param name="message"></param>
+        void ShowMessage(string message);
+
+        /// <summary>
+        /// Путь к файлу.
+        /// </summary>
+        string FilePath { get; set; }
+
+        /// <summary>
+        /// Пытается открыть диалоговое окно открытия файла. В случае удачи
+        /// возвращает true, иначе - false.
+        /// </summary>
+        /// <returns>Удалось ли открыть диалоговое окно (true) или нет
+        /// (false).
+        /// </returns>
+        bool OpenFileDialog();
+
+        /// <summary>
+        /// Пытается открыть диалоговое окно сохранения файла. В случае удачи
+        /// возвращает true, иначе - false.
+        /// </summary>
+        /// <returns>Удалось ли открыть диалоговое окно (true) или нет
+        /// (false).
+        /// </returns>
+        bool SaveFileDialog();
+    }
+}
