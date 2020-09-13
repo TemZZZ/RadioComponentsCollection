98d62f22a7ce59cb02fe55a6eb4cb7acd88d3175
TemZZZ <temsky.v.tomske@gmail.com>
2020-09-14 01:59:07 +0700

    feature(project MVVM, SearchRadiocomponentViewModel): добавлен конструктор с параметрами
    - передаются коллекция радиокомпонентов, по которым производится поиск, и допустимые типы для поиска

--- MVVM/SearchRadiocomponentViewModel.cs
+++ MVVM/SearchRadiocomponentViewModel.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using Model;
 
 namespace MVVM
 {
@@ -11,5 +8,33 @@ namespace MVVM
     /// </summary>
     internal class SearchRadiocomponentViewModel : ViewModelBase
     {
+        #region -- Private fields --
+
+        private IEnumerable<RadiocomponentType>
+            _availableRadiocomponentTypes;
+        private IEnumerable<RadiocomponentToPrintableRadiocomponentAdapter>
+            _radiocomponents;
+
+        #endregion
+
+        #region -- Constructors --
+
+        /// <summary>
+        /// Создает экземпляр модели представления поиска радиокомпонентов.
+        /// </summary>
+        /// <param name="availableRadiocomponentTypes">Доступные типы
+        /// радиокомпонентов для поиска.</param>
+        /// <param name="radiocomponents">Коллекция радиокомпонентов, по
+        /// которой производится поиск.</param>
+        public SearchRadiocomponentViewModel(
+            IEnumerable<RadiocomponentType> availableRadiocomponentTypes,
+            IEnumerable<RadiocomponentToPrintableRadiocomponentAdapter>
+                radiocomponents)
+        {
+            _availableRadiocomponentTypes = availableRadiocomponentTypes;
+            _radiocomponents = radiocomponents;
+        }
+
+        #endregion
     }
 }
