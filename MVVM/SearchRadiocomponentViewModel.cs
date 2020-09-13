393c2624a39f1e37330a03208536e6f62adeaa39
TemZZZ <temsky.v.tomske@gmail.com>
2020-09-14 01:31:27 +0700

    feature(project MVVM): добавлен класс SearchRadiocomponentViewModel без реализации

---
+++ MVVM/SearchRadiocomponentViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM
+{
+    /// <summary>
+    /// Класс модели представления поиска радиокомпонентов по коллекции.
+    /// </summary>
+    internal class SearchRadiocomponentViewModel : ViewModelBase
+    {
+    }
+}
