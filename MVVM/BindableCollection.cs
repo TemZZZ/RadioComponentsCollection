93378319fa7ec056b4b129c3311b921d31782dbc
TemZZZ <temsky.v.tomske@gmail.com>
2020-09-25 18:48:03 +0700

    feature(project MVVM): добавлен и реализован класс коллекции BindableCollection<T>
    - можно свойству класса T назначить атрибут [Browsable(false)], и это свойство не будет отображаться в DataGrid

---
+++ MVVM/BindableCollection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace MVVM
+{
+    /// <summary>
+    /// Класс коллекции с возможностью привязки к ней, а также с возможностью
+    /// скрывать видимость публичных свойств, например, в DataGrid с помощью
+    /// атрибута <see cref="BrowsableAttribute"/>.
+    /// </summary>
+    /// <typeparam name="T">Тип элементов коллекции.</typeparam>
+    public class BindableCollection<T> : ObservableCollection<T>, ITypedList
+    {
+        /// <summary>
+        /// Массив элементов типа <see cref="Attribute"/>, используемых в
+        /// качестве фильтров элементов коллекции.
+        /// </summary>
+        private readonly Attribute[] _propertyFilter =
+        {
+            BrowsableAttribute.Yes
+        };
+
+        /// <inheritdoc/>
+        public string GetListName(PropertyDescriptor[] listAccessors)
+        {
+            return null;
+        }
+
+        /// <inheritdoc/>
+        public PropertyDescriptorCollection GetItemProperties(
+            PropertyDescriptor[] listAccessors)
+        {
+            return TypeDescriptor.GetProperties(typeof(T), _propertyFilter);
+        }
+    }
+}
