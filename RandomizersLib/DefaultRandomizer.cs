dce93af741e03b6a00cdae7d27ad3f02015540a0
TemZZZ <temsky.v.tomske@gmail.com>
2020-09-29 17:56:57 +0700

    feature(project RandomizerLib): добавлен и реализован класс стандартного рандомизатора DefaultRandomizer

---
+++ RandomizersLib/DefaultRandomizer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RandomizersLib
+{
+    /// <summary>
+    /// Класс стандартного рандомизатора, реализованного на базе класса
+    /// Random.
+    /// </summary>
+    public class DefaultRandomizer : IRandomizer
+    {
+        private readonly Random _random = new Random();
+
+        /// <inheritdoc/>
+        public int Next()
+        {
+            return _random.Next();
+        }
+
+        /// <inheritdoc/>
+        public int Next(int maxValue)
+        {
+            return _random.Next(maxValue);
+        }
+    }
+}
