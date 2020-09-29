3e88b75d618a4cbe43b9bf44d0558e7965624457
TemZZZ <temsky.v.tomske@gmail.com>
2020-09-29 14:15:33 +0700

    refactor(poject MVVM, IDialogService): изменены сигнатуры методов OpenFileDialog и SaveFileDialog
    - теперь методы принимают параметры string defaultExtension, string filter

--- MVVM/IDIalogService.cs
+++ MVVM/IDIalogService.cs
@@ -21,16 +21,22 @@
         /// Пытается открыть диалоговое окно открытия файла. В случае удачи
         /// возвращает true, иначе - false.
         /// </summary>
+        /// <param name="defaultExtension">Стандартное расширение.</param>
+        /// <param name="filter">Расширения файлов, используемые в качестве
+        /// фильтров отображаемых файлов в диалоговом окне.</param>
         /// <returns>Если пользователь кликнул по кнопке OK в диалоговом
         /// окне, то возвращается true, иначе - false. </returns>
-        bool OpenFileDialog();
+        bool OpenFileDialog(string defaultExtension, string filter);
 
         /// <summary>
         /// Пытается открыть диалоговое окно сохранения файла. В случае удачи
         /// возвращает true, иначе - false.
         /// </summary>
+        /// <param name="defaultExtension">Стандартное расширение.</param>
+        /// <param name="filter">Расширения файлов, используемые в качестве
+        /// фильтров отображаемых файлов в диалоговом окне.</param>
         /// <returns>Если пользователь кликнул по кнопке OK в диалоговом
         /// окне, то возвращается true, иначе - false. </returns>
-        bool SaveFileDialog();
+        bool SaveFileDialog(string defaultExtension, string filter);
     }
 }
