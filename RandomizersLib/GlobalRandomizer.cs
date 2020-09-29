e8e69d7f34e4d505c2b686599f5e023d54f1ca22
TemZZZ <temsky.v.tomske@gmail.com>
2020-09-29 18:10:14 +0700

    feature(project RandomizersLib): добавлен и реализован класс GlobalRandomizer, выполняющий роль контекста окружения для классов рандомизаторов

---
+++ RandomizersLib/GlobalRandomizer.cs
@@ -0,0 +1,18 @@
+﻿namespace RandomizersLib
+{
+    /// <summary>
+    /// Реализация паттерна Ambient Context (контекст окружения, одна из
+    /// вариаций паттерна "Синглтон") для возможности установки нужного
+    /// класса рандомизатора.
+    /// </summary>
+    public class GlobalRandomizer
+    {
+        /// <summary>
+        /// Позволяет получить или задать экземпляр рандомизатора. По
+        /// умолчанию возвращает экземпляр класса
+        /// <see cref="DefaultRandomizer"/>.
+        /// </summary>
+        public static IRandomizer Instance { get; internal set; }
+            = new DefaultRandomizer();
+    }
+}
