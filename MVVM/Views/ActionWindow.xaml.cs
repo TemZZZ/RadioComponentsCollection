c22ff3bdfe352d1231c48d913eed8e1fb6abe750
TemZZZ <temsky.v.tomske@gmail.com>
2020-10-02 11:32:20 +0700

    refactor(project MVVM): все представления (XAML файлы, кроме App.xaml) перенесены в папку Views

---
+++ MVVM/Views/ActionWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace MVVM.Views
+{
+    /// <summary>
+    /// Interaction logic for ActionWindow.xaml
+    /// </summary>
+    public partial class ActionWindow : Window
+    {
+        public ActionWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MVVM/Views/AddRadiocomponentWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace MVVM.Views
+{
+    /// <summary>
+    /// Interaction logic for AddRadiocomponentView.xaml
+    /// </summary>
+    public partial class AddRadiocomponentWindow : Window
+    {
+        public AddRadiocomponentWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
