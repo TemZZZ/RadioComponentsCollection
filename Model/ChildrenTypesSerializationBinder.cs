f8a2ea88ff3884f49522f4d83d9965e73b5696d8
TemZZZ <temsky.v.tomske@gmail.com>
2020-10-05 15:31:21 +0700

    feature(project Model): добавлена NuGet библиотека Newtonsoft Json
    - добавлен и реализован класс ChildrenTypesSerializationBinder, который будет использоваться для сериализации

---
+++ Model/ChildrenTypesSerializationBinder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Model
+{
+    using Newtonsoft.Json.Serialization;
+
+    /// <summary>
+    /// Класс-связыватель дочерних типов одного супер-класса с сериализуемыми
+    /// объектами.
+    /// </summary>
+    public class ChildrenTypesSerializationBinder : ISerializationBinder
+    {
+        private IEnumerable<Type> _childrenTypes;
+
+        /// <summary>
+        /// Создает экземпляр класса-связывателя дочерних типов одного
+        /// супер-класса с сериализуемыми объектами.
+        /// </summary>
+        /// <param name="baseType">Супер-класс, дочерние типы которого могут
+        /// быть связаны с сериализуемыми объектами.</param>
+        public ChildrenTypesSerializationBinder(Type baseType)
+        {
+            _childrenTypes = Assembly.GetAssembly(baseType).GetTypes()
+                .Where(type => type.IsSubclassOf(baseType));
+        }
+
+        /// <inheritdoc/>
+        public Type BindToType(string assemblyName, string typeName)
+        {
+            return _childrenTypes.SingleOrDefault(
+                type => type.Name == typeName);
+        }
+
+        /// <inheritdoc/>
+        public void BindToName(Type serializedType, out string assemblyName,
+            out string typeName)
+        {
+            assemblyName = null;
+            typeName = serializedType.Name;
+        }
+    }
+}
