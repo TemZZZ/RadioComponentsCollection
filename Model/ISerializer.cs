0ab4d85ff632aca9bebb4db5a2ab87b8d7935e3c
TemZZZ <temsky.v.tomske@gmail.com>
2020-10-05 16:43:10 +0700

    feature(project Model): добавлен и описан интерфейс сериализатора ISerializer

---
+++ Model/ISerializer.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+namespace Model
+{
+    /// <summary>
+    /// Интерфейс сериализатора.
+    /// </summary>
+    public interface ISerializer
+    {
+        /// <summary>
+        /// Сериализует объект и передает его в поток вывода.
+        /// </summary>
+        /// <param name="stream">Поток вывода.</param>
+        /// <param name="serializingObject">Сериализуемый объект.</param>
+        void Serialize(StreamWriter stream, object serializingObject);
+
+        /// <summary>
+        /// Десериализует объект, полученный из потока ввода.
+        /// </summary>
+        /// <param name="stream">Поток ввода.</param>
+        void Deserialize(StreamReader stream);
+    }
+}
