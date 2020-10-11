d581ccf8f837e57f9c8db2436f078421d4cd4edb
TemZZZ <temsky.v.tomske@gmail.com>
2020-10-12 00:39:49 +0700

    style(project Model, RadiocomponentBase): оформлен код
    - удалены атрибуты XmlInclude за ненадобностью
    - добавлены точки в конце XML комментариев

--- Model/RadiocomponentBase.cs
+++ Model/RadiocomponentBase.cs
@@ -1,31 +1,27 @@
 ﻿using System.Numerics;
-using System.Xml.Serialization;
 
 namespace Model
 {
 	/// <summary>
-	/// Абстрактный класс радиокомпонента.
-	/// Его наследуют все производные классы радиокомпонентов
+	/// Абстрактный класс радиокомпонента. Его наследуют все производные
+	/// классы радиокомпонентов.
 	/// </summary>
-	[XmlInclude(typeof(Resistor))]
-	[XmlInclude(typeof(Inductor))]
-	[XmlInclude(typeof(Capacitor))]
 	public abstract class RadiocomponentBase : IRadiocomponent
 	{
 		private double _value;
 
 		/// <summary>
-		/// Создает экземпляр класса <see cref="RadiocomponentBase"/>
+		/// Создает экземпляр класса <see cref="RadiocomponentBase"/>.
 		/// </summary>
-		/// <param name="value">Значение физической величины в СИ</param>
+		/// <param name="value">Значение физической величины в СИ.</param>
 		protected RadiocomponentBase(double value)
 		{
 			Value = value;
 		}
 
 		/// <summary>
-		/// Позволяет получить или присвоить значение
-		/// физической величины радиокомпонента
+		/// Позволяет получить или присвоить значение физической величины
+		/// радиокомпонента.
 		/// </summary>
 		public double Value
 		{
@@ -34,45 +30,45 @@ namespace Model
             {
                 const string parameterName
                     = "Physical value of radiocomponent";
-                RadiocomponentService.ValidatePositiveDouble(
-                    value, parameterName);
+                RadiocomponentService.ValidatePositiveDouble(value,
+                    parameterName);
                 _value = value;
             }
 		}
 
 		/// <summary>
-		/// Возвращает частотнозависимый комплексный
-		/// импеданс радиокомпонента
+		/// Возвращает частотнозависимый комплексный импеданс
+		/// радиокомпонента.
 		/// </summary>
 		public Complex GetImpedance(double frequency)
 		{
-            RadiocomponentService.ValidatePositiveDouble(
-                frequency, nameof(frequency));
+            RadiocomponentService.ValidatePositiveDouble(frequency,
+                nameof(frequency));
             return CalculateImpedance(frequency);
 		}
 
 		/// <summary>
-		/// Возвращает строковое представление объекта
+		/// Возвращает строковое представление радиокомпонента.
 		/// </summary>
 		public override string ToString()
 		{
             return RadiocomponentService.ToString(Type, Value);
 		}
 
-        /// <inheritdoc />
+        /// <inheritdoc/>
         public RadiocomponentQuantity Quantity
             => RadiocomponentService.GetRadiocomponentQuantity(Type);
 
-        /// <inheritdoc />
+        /// <inheritdoc/>
         public RadiocomponentUnit Unit
             => RadiocomponentService.GetRadiocomponentUnit(Type);
 
 		/// <summary>
-		/// Вычисляет частотнозависимый комплексный импеданс
-		/// и возвращает значение
+		/// Вычисляет частотнозависимый комплексный импеданс и возвращает
+		/// значение импеданса.
 		/// </summary>
-		/// <param name="frequency">Частота в герцах</param>
-		/// <returns>Комплексный импеданс в омах</returns>
+		/// <param name="frequency">Частота в герцах.</param>
+		/// <returns>Комплексный импеданс в омах.</returns>
 		protected abstract Complex CalculateImpedance(double frequency);
 
 		/// <inheritdoc/>
