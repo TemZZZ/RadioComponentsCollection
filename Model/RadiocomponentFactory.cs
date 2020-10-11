7edd0f02460e2bc33e194cd50ad889a7ee4706ab
TemZZZ <temsky.v.tomske@gmail.com>
2020-10-12 00:28:21 +0700

    refactor+style(project Model, RadiocomponentFactory.GetTypeToRadiocomponentDictionary): переименован из GetTypeToRadiocomponentMap
    - добавлены точки в конце предложений в XML комментариях

--- Model/RadiocomponentFactory.cs
+++ Model/RadiocomponentFactory.cs
@@ -5,13 +5,13 @@ using Randomizers;
 namespace Model
 {
 	/// <summary>
-	/// Класс фабрики радиокомпонентов
+	/// Класс фабрики радиокомпонентов.
 	/// </summary>
 	public static class RadiocomponentFactory
 	{
 		/// <summary>
 		/// Словарь соответствий типа <see cref="Type"/> радиокомпонента и
-		/// элемента из перечисления <see cref="RadiocomponentType"/>
+		/// элемента из перечисления <see cref="RadiocomponentType"/>.
 		/// </summary>
 		private static readonly Dictionary<Type, RadiocomponentType>
 			_typeToRadiocomponentTypeMap
@@ -37,51 +37,51 @@ namespace Model
 
 		/// <summary>
 		/// Возвращает словарь пар значений
-		/// "тип радиокомпонента-радиокомпонент"
+		/// "тип радиокомпонента-радиокомпонент".
 		/// </summary>
 		/// <param name="radiocomponentValue">Значение физической величины
-		/// радиокомпонента</param>
+		/// радиокомпонента.</param>
 		/// <returns>Словарь пар значений
-		/// "тип радиокомпонента-радиокомпонент"
+		/// "тип радиокомпонента-радиокомпонент".
 		/// </returns>
 		private static Dictionary<RadiocomponentType, RadiocomponentBase>
-				GetTypeToRadiocomponentMap(double radiocomponentValue)
-		{
-			return new Dictionary<RadiocomponentType, RadiocomponentBase>
-			{
-				[RadiocomponentType.Resistor]
-					= new Resistor(radiocomponentValue),
-				[RadiocomponentType.Inductor]
-					= new Inductor(radiocomponentValue),
-				[RadiocomponentType.Capacitor]
-					= new Capacitor(radiocomponentValue)
-			};
+            GetTypeToRadiocomponentDictionary(double radiocomponentValue)
+        {
+            return new Dictionary<RadiocomponentType, RadiocomponentBase>
+            {
+                [RadiocomponentType.Resistor]
+                    = new Resistor(radiocomponentValue),
+                [RadiocomponentType.Inductor]
+                    = new Inductor(radiocomponentValue),
+                [RadiocomponentType.Capacitor]
+                    = new Capacitor(radiocomponentValue)
+            };
 		}
 
 		/// <summary>
 		/// Возвращает радиокомпонент определенного типа с требуемым
-		/// значением физической величины
+		/// значением физической величины.
 		/// </summary>
-		/// <param name="radiocomponentType">Тип радиокомпонента</param>
+		/// <param name="radiocomponentType">Тип радиокомпонента.</param>
 		/// <param name="radiocomponentValue">Значение физической величины
-		/// радиокомпонента</param>
+		/// радиокомпонента.</param>
 		/// <returns>Объект класса-наследника
-		/// <see cref="RadiocomponentBase"/></returns>
+		/// <see cref="RadiocomponentBase"/>.</returns>
 		/// <exception cref="KeyNotFoundException"/>
 		public static RadiocomponentBase GetRadiocomponent(
 			RadiocomponentType radiocomponentType,
 			double radiocomponentValue)
 		{
-			return GetTypeToRadiocomponentMap(radiocomponentValue)
+			return GetTypeToRadiocomponentDictionary(radiocomponentValue)
 				[radiocomponentType];
 		}
 
 		/// <summary>
 		/// Возвращает тип <see cref="RadiocomponentType"/> переданного
-		/// радиокомпонента
+		/// радиокомпонента.
 		/// </summary>
-		/// <param name="radiocomponent">Радиокомпонент</param>
-		/// <returns>Тип радиокомпонента</returns>
+		/// <param name="radiocomponent">Радиокомпонент.</param>
+		/// <returns>Тип радиокомпонента.</returns>
 		public static RadiocomponentType GetRadiocomponentType(
 			IRadiocomponent radiocomponent)
 		{
