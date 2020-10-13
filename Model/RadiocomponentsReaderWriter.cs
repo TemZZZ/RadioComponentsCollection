48d5bca19da9a61811001aa3936ad730c1878463
TemZZZ <temsky.v.tomske@gmail.com>
2020-10-13 16:08:33 +0700

    feature(project MVVM, RadiocomponentsReaderWriter): удалены DefaultExtension, DefaultFilesFilter, SaveOptionToDescriptionDictionary, LoadOptionToDescriptionDictionary за ненадобностью

--- Model/RadiocomponentsReaderWriter.cs
+++ Model/RadiocomponentsReaderWriter.cs
@@ -25,42 +25,6 @@ namespace Model
         }
 
         /// <summary>
-        /// Стандартное расширение файлов, хранящих радиокомпоненты.
-        /// </summary>
-        public const string DefaultExtension = "cmp";
-
-        /// <summary>
-        /// Стандартный фильтр файлов.
-        /// </summary>
-        public static string DefaultFilesFilter
-            => $"Файлы радиокомпонентов (*.{DefaultExtension})|" +
-               $"*.{DefaultExtension}|Все файлы (*.*)|*.*";
-
-        /// <summary>
-        /// Опции сохранения радиокомпонентов в файл с описаниями.
-        /// </summary>
-        public static Dictionary<SaveOption, string>
-            SaveOptionToDescriptionDictionary
-            => new Dictionary<SaveOption, string>
-            {
-                [SaveOption.SaveAll] = "Сохранить все радиокомпоненты",
-                [SaveOption.SaveSelected]
-                    = "Сохранить только выделенные радиокомпоненты"
-            };
-
-        /// <summary>
-        /// Опции загрузки радиокомпонентов из файла с описаниями.
-        /// </summary>
-        public static Dictionary<LoadOption, string>
-            LoadOptionToDescriptionDictionary
-            => new Dictionary<LoadOption, string>
-            {
-                [LoadOption.AddToEnd] = "Добавить в конец таблицы",
-                [LoadOption.ReplaceAll]
-                    = "Заменить все радиокомпоненты в таблице новыми"
-            };
-
-        /// <summary>
         /// Сохраняет радиокомпоненты в файл.
         /// </summary>
         /// <param name="saveOption">Опция сохранения в файл.</param>
