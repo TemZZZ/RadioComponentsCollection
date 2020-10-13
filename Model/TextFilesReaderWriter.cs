8a2bacf0e84a243b9b7c881ad637539ecf402574
TemZZZ <temsky.v.tomske@gmail.com>
2020-10-13 15:12:22 +0700

    refactor(proejct Model, TextFilesReaderWriter): в методах ReadFileAndDeserialize и SerializeAndWriteToFile используются TextFileIOService.GetStreamReader и TextFileIOService.GetStreamWriter вместо местных приватных одноименных методов
    - удалены приватные методы GetStreamWriter, GetStreamReader за ненадобностью
    - удалено приватное поле _fileIoExceptionTypeToErrorMessageDictionary за ненадобностью

--- Model/TextFilesReaderWriter.cs
+++ Model/TextFilesReaderWriter.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 using Model.Serializers;
 
 namespace Model
@@ -11,34 +9,7 @@ namespace Model
 	/// </summary>
 	public class TextFilesReaderWriter
 	{
-		#region -- Private fields --
-
         private readonly ISerializer _serializer;
-
-		/// <summary>
-        /// Возможные в процессе работы с файлами типы исключений и
-        /// соответствующие им сообщения об ошибках.
-        /// </summary>
-        private readonly Dictionary<Type, string>
-            _fileIoExceptionTypeToErrorMessageDictionary
-                = new Dictionary<Type, string>
-                {
-                    [typeof(ArgumentNullException)]
-                        = "Имя файла не может быть пустым.",
-                    [typeof(DirectoryNotFoundException)]
-                        = "Не удается найти часть файла или каталога.",
-                    [typeof(PathTooLongException)]
-                        = "Слишком длинный путь к файлу или его имя.",
-                    [typeof(UnauthorizedAccessException)]
-                        = "Доступ к файлу запрещен. Возможно, у Вас не " +
-                          "достаточно прав для доступа к файлу.",
-                    [typeof(FileNotFoundException)] = "Файл не найден.",
-                    [typeof(IOException)]
-                        = "Доступ к файлу запрещен. Возможно, он " +
-                          "используется другим процессом."
-                };
-
-		#endregion
 
         #region -- Constructors --
 
@@ -52,42 +23,6 @@ namespace Model
         }
 
         #endregion
-
-		#region -- Auxiliary private methods --
-
-        /// <summary>
-        /// Создает или перезаписывает текстовый файл по указанному пути.
-        /// </summary>
-        /// <param name="fileName">Путь к файлу.</param>
-        /// <param name="errorMessager">Делегат для передачи сообщений об
-        /// ошибках.</param>
-        /// <returns>Объект <see cref="StreamWriter"/> или null.</returns>
-        private StreamWriter GetStreamWriter(string fileName,
-            Action<string> errorMessager = null)
-        {
-            return ExceptionHandler.CallFunction(File.CreateText, fileName,
-                _fileIoExceptionTypeToErrorMessageDictionary, errorMessager);
-        }
-
-        /// <summary>
-        /// Инициализирует новый экземпляр класса <see cref="StreamReader"/>
-        /// для указанного имени файла.
-        /// </summary>
-        /// <param name="fileName">Путь к файлу.</param>
-        /// <param name="errorMessager">Делегат для передачи сообщений об
-        /// ошибках.</param>
-        /// <returns>Объект <see cref="StreamReader"/> или null.</returns>
-        private StreamReader GetStreamReader(string fileName,
-            Action<string> errorMessager = null)
-        {
-            Func<string, StreamReader> CreateStreamReader =
-                _fileName => new StreamReader(_fileName);
-            return ExceptionHandler.CallFunction(CreateStreamReader,
-                fileName, _fileIoExceptionTypeToErrorMessageDictionary,
-                errorMessager);
-        }
-
-		#endregion
 
 		#region -- Public methods --
 
@@ -103,7 +38,8 @@ namespace Model
 		public void SerializeAndWriteToFile<T>(T serializingObject,
 			string fileName, Action<string> errorMessager = null)
 		{
-			var file = GetStreamWriter(fileName, errorMessager);
+			var file = TextFileIOService.GetStreamWriter(fileName,
+                errorMessager);
 			if (file is null)
 			{
 				return;
@@ -146,7 +82,8 @@ namespace Model
 		public T ReadFileAndDeserialize<T>(string fileName,
 			Action<string> errorMessager = null)
 		{
-			var file = GetStreamReader(fileName, errorMessager);
+			var file = TextFileIOService.GetStreamReader(fileName,
+                errorMessager);
 			if (file is null)
 			{
 				return default;
