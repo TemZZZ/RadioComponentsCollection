1791292542bd8eb53a509b321fba069d6950d89d
TemZZZ <temsky.v.tomske@gmail.com>
2020-10-13 16:24:43 +0700

    feature(project MVVM): класс RadiocomponentsReaderWriter разделен на два класса
    - добавлен и реализован класс RadiocomponentsReader, в который скопирован метод LoadFromFile из класса RadiocomponentsReaderWriter
    - добавлен и реализован класс RadiocomponentsWriter, в который скопирован метод SaveToFile из класса RadiocomponentsReaderWriter

---
+++ Model/RadiocomponentsWriter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Model
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class RadiocomponentsWriter
+    {
+        private readonly TextFileWriter _textFileWriter;
+
+        /// <summary>
+        /// Создает экземпляр класса чтения радиокомпонентов из файла.
+        /// </summary>
+        /// <param name="textFileWriter">Экземпляр класса записи объектов в
+        /// текстовые файлы <see cref="TextFileWriter"/>.</param>
+        public RadiocomponentsWriter(TextFileWriter textFileWriter)
+        {
+            _textFileWriter = textFileWriter;
+        }
+
+        /// <summary>
+        /// Сохраняет радиокомпоненты в файл.
+        /// </summary>
+        /// <param name="saveOption">Опция сохранения в файл.</param>
+        /// <param name="filePath">Путь к файлу.</param>
+        /// <param name="allRadiocomponents">Все радиокомпоненты.</param>
+        /// /// <param name="selectedRadiocomponents">Выделенные
+        /// радиокомпоненты.</param>
+        /// /// <param name="errorMessager">Делегат для вывода сообщений об
+        /// ошибках.</param>
+        /// <returns>true, если радиокомпоненты были сохранены, иначе -
+        /// false.</returns>
+        public bool SaveToFile(SaveOption saveOption, string filePath,
+            IList<RadiocomponentBase> allRadiocomponents,
+            IList<RadiocomponentBase> selectedRadiocomponents,
+            Action<string> errorMessager = null)
+        {
+            IEnumerable<RadiocomponentBase> savingRadiocomponents;
+
+            switch (saveOption)
+            {
+                case SaveOption.SaveAll:
+                    savingRadiocomponents = allRadiocomponents;
+                    break;
+                case SaveOption.SaveSelected:
+                    savingRadiocomponents = selectedRadiocomponents;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(saveOption),
+                        saveOption, null);
+            }
+
+            _textFileWriter.SerializeAndWriteToFile(savingRadiocomponents,
+                filePath, errorMessager);
+            return true;
+        }
+    }
+}
